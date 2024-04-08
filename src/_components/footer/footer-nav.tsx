ae888680d95aab2bc23362847d08e82eeee40e8d
bytegenius01 <git config --global user.email>
2024-04-08 19:20:00 +0500

    feat: border

--- src/_components/footer/footer-nav.tsx
+++ src/_components/footer/footer-nav.tsx
@@ -1,26 +1,29 @@
 import Link from "next/link";
 export default function FooterNav() {
   return (
-    <div className="col-start-1 flex items-center justify-between text-white font-semibold text-sm lg:text-base">
-      <span className="px-4 py-2">
-        <Link href={"#"}>About Us</Link>
-      </span>
-      <span className="w-1 bg-white h-6" />
-      <span className="px-4 py-2">
-        <Link href={"#"}>Personal Data Policy</Link>
-      </span>
-      <span className="w-1 bg-white h-6" />
-      <span className="px-4 py-2">
-        <Link href={"#"}>Cookies Policy</Link>
-      </span>
-      <span className="w-1 bg-white h-6" />
-      <span className="px-4 py-2">
-        <Link href={"#"}>Terms and conditions</Link>
-      </span>
-      <span className="w-1 bg-white h-6" />
-      <span className="px-4 py-2">
-        <Link href={"#"}>Contact Us</Link>
-      </span>
-    </div>
+    <>
+      <div className="col-start-1 flex items-center justify-between text-white font-semibold text-sm lg:text-base mb-8">
+        <span className="px-4 py-2">
+          <Link href={"#"}>About Us</Link>
+        </span>
+        <span className="w-1 bg-white h-6" />
+        <span className="px-4 py-2">
+          <Link href={"#"}>Personal Data Policy</Link>
+        </span>
+        <span className="w-1 bg-white h-6" />
+        <span className="px-4 py-2">
+          <Link href={"#"}>Cookies Policy</Link>
+        </span>
+        <span className="w-1 bg-white h-6" />
+        <span className="px-4 py-2">
+          <Link href={"#"}>Terms and conditions</Link>
+        </span>
+        <span className="w-1 bg-white h-6" />
+        <span className="px-4 py-2">
+          <Link href={"#"}>Contact Us</Link>
+        </span>
+      </div>
+      <span className="border-b border-neutral-50 col-span-2 mb-32" />
+    </>
   );
 }
