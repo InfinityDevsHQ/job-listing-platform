c354e49e60cc0cdd10ba2078099ae26afac8df84
bytegenius01 <git config --global user.email>
2024-04-15 15:52:27 +0500

    refactor: made components for all svgs

--- src/_components/jobs-section/_components/job-card-header.tsx
+++ src/_components/jobs-section/_components/job-card-header.tsx
@@ -1,3 +1,4 @@
+import CompanyBookmark from "@/components/svgs/company-bookmark";
 import { Plane } from "lucide-react";
 type JobCardHeaderProps = {
   jobTitle: string;
@@ -41,20 +42,7 @@ export default function JobCardHeader({
           className="flex items-center justify-center w-30 h-30 border border-neutral-300"
         >
           <span className="px-[11.21px]">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-4 h-4 text-neutral-300"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
-              />
-            </svg>
+            <CompanyBookmark />
           </span>
         </button>
       </div>
