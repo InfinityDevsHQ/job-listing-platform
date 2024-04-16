c58c7987e9ae07308fb75d5e14b5e622e9d41696
bytegenius01 <git config --global user.email>
2024-04-16 18:30:23 +0500

    feat: apps auth svgs

--- src/_components/apps-auth.tsx
+++ src/_components/apps-auth.tsx
@@ -1,3 +1,6 @@
+import CompanyGithub from "@/components/svgs/company-github-second";
+import CompanyGoogle from "@/components/svgs/company-google";
+import CompanyLinkedin from "@/components/svgs/company-linedin";
 import { Button } from "@/components/ui/button";
 type AppsAuthProps = {
   google?: boolean;
@@ -14,20 +17,20 @@ export default function AppsAuth({
   return (
     <div className="flex items-center justify-center gap-4">
       {google && (
-        <Button className="flex gap-2 bg-neutral-50 px-4 py-2 border border-neutral-200 text-black font-sans leading-6 font-medium lg:text-15">
-          <span></span>
+        <Button className="flex gap-2 items-center bg-neutral-50 px-4 py-2 border border-neutral-200 text-black font-sans leading-6 font-medium lg:text-15">
+          <CompanyGoogle />
           <span>Google</span>
         </Button>
       )}
       {github && (
-        <Button className="flex gap-2 bg-neutral-50 px-4 py-2 border border-neutral-200 text-black font-sans leading-6 font-medium lg:text-15">
-          <span></span>
+        <Button className="flex gap-2 items-center bg-neutral-50 px-4 py-2 border border-neutral-200 text-black font-sans leading-6 font-medium lg:text-15">
+          <CompanyGithub />
           <span>Github</span>
         </Button>
       )}
       {linkedin && (
-        <Button className="flex gap-2 bg-neutral-50 px-4 py-2 border border-neutral-200 text-black font-sans leading-6 font-medium lg:text-15">
-          <span></span>
+        <Button className="flex gap-2 items-center bg-neutral-50 px-4 py-2 border border-neutral-200 text-black font-sans leading-6 font-medium lg:text-15">
+          <CompanyLinkedin />
           <span>Linkedin</span>
         </Button>
       )}
