c087f4401a85ff895485bbcc641829f9592d2b38
bytegenius01 <git config --global user.email>
2024-04-17 21:42:25 +0500

    fix: added missing username input

--- src/app/register/_components/register-candidate-inputs.tsx
+++ src/app/register/_components/register-candidate-inputs.tsx
@@ -2,9 +2,19 @@ import CompanyMail from "@/components/svgs/coompany-mail";
 import CompanyLock from "@/components/svgs/company-lock";
 import CompanyEye from "@/components/svgs/company-eye";
 import { Input } from "@/components/ui/input";
+import CompanyProfileOne from "@/components/svgs/company-profile-one";
 export default function RegisterCandidateInputs() {
   return (
     <>
+      <div className="px-3 py-2 flex items-center gap-2.5 bg-white w-full border-gray-300">
+        <CompanyProfileOne />
+        <Input
+          className="w-full border-none placeholder:text-gray-400 !text-black"
+          placeholder="Username"
+          name="username"
+          type="text"
+        />
+      </div>
       <div className="px-3 py-2 flex items-center gap-2.5 bg-white w-full border-gray-300">
         <CompanyMail width={14} height={11} />
         <Input
