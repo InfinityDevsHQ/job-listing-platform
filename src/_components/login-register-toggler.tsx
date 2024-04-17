83f173096a413d0761cf22f6288f8da8c98296be
bytegenius01 <git config --global user.email>
2024-04-17 14:23:03 +0500

    fix: add some padding sso form does not touch boundaries

--- src/_components/login-register-toggler.tsx
+++ src/_components/login-register-toggler.tsx
@@ -6,7 +6,7 @@ export default function LoginRegisterToggler({
   currentPage,
 }: LoginRegisterTogglerProps) {
   return (
-    <div className="flex items-center justify-between">
+    <div className="flex items-center justify-between mb-8">
       <span className="font-sans px-4 py-2 font-medium text-16 leading-6 !text-gray-200">
         {currentPage === "register"
           ? `Already Have Account ?`
