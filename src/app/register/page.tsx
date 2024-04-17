d11bc3ce2c1aa9b53ab0261a65ade9791789a3dc
bytegenius01 <git config --global user.email>
2024-04-17 17:21:08 +0500

    cleaup: unecessary comment

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -37,8 +37,6 @@ export default function Register() {
           <AppsAuth google linkedin github auth="register" />
           <VectorText text="or" />
           <TabNavigator tabs={Tabs} />
-          {/* <InputsToggler open={opened} registerSetOpen={setOpened} /> */}
-
           {opened === "Company" ? (
             <RegisterCompanyInputs />
           ) : (
