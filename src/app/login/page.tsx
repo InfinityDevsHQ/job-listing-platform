ac74e2f7d4e79d101aa9c399af4e0f8407c3086d
bytegenius01 <git config --global user.email>
2024-04-17 15:26:01 +0500

    fix: background colors

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,7 +12,7 @@ export default function Login() {
   );
   return (
     <main className="grid grid-cols-2 min-h-screen">
-      <div className="col-span-2 lg:col-span-1 flex-center lg:block flex-col px-3.9 lg:px-0 bg-primary lg:bg-red-200">
+      <div className="col-span-2 lg:col-span-1 flex-center lg:block flex-col px-3.9 lg:px-0 bg-primary-900 lg:bg-primary-100">
         <FormsHeader />
         <LoginForm
           loginData={loginData}
