40f6d3828665e112539310b257469297479ed357
Saad Gulzar <isaadgulzar@gmail.com>
2024-04-19 04:23:21 +0500

    feat: update job card design

--- src/_components/jobs-section/_components/job-card-details.tsx
+++ src/_components/jobs-section/_components/job-card-details.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import CompanyApplied from "@/components/svgs/company-applied";
 import CompanyCalender from "@/components/svgs/company-calender";
 import CompanyClock from "@/components/svgs/company-clock";
@@ -25,24 +27,63 @@ export default function JobCardDetails({
   return (
     <>
       <div className="flex flex-col lg:flex-row gap-4">
-        <div className="flex items-center justify-between  flex-1">
-          <span className="flex items-center gap-2.5 font-sans text-10 font-semibold leading-3">
-            <CompanyClock width={14} height={12} />
-            <span className="text-10 lg:text-sm lg:leading-5">{jobType}</span>
+        <div className="flex items-center justify-between flex-wrap">
+          <span className="flex items-center gap-2.5">
+            <Image
+              alt="timer"
+              src={"/assets/icons/timer.svg"}
+              width={14}
+              height={12}
+            />
+            <span className="text-xxs lg:text-sm font-semibold lg:font-normal text-gray-950">{jobType}</span>
           </span>
-          <span className="flex items-center gap-2.5 font-sans text-10 lg:text-sm lg:leading-5 font-semibold leading-3">
-            <CompanyExperience width={14} height={12} />
-            <span className="text-10 lg:text-sm lg:leading-5">
-              {experienceRequired}
-            </span>
+          <span className="flex items-center gap-2.5">
+            <Image
+              alt="briefcase"
+              src={"/assets/icons/briefcase.svg"}
+              width={14}
+              height={12}
+            />
+            <span className="text-xxs lg:text-sm font-semibold lg:font-normal text-gray-950">{experienceRequired}</span>
           </span>
-          <span className="flex items-center gap-2.5 font-sans text-10 lg:text-sm lg:leading-5 font-semibold leading-5">
-            <CompanyLocation width={11} height={12} />
-            <span className="text-10 lg:text-sm lg:leading-5">{location}</span>
+          <span className="flex items-center gap-2.5">
+            <Image
+              alt="map-marker-alt"
+              src={"/assets/icons/map-marker-alt.svg"}
+              width={11}
+              height={12}
+            />
+            <span className="text-xxs lg:text-sm font-semibold lg:font-normal text-gray-950">{location}</span>
+          </span>
+          <span className="flex items-center gap-2.5">
+            <Image
+              alt="circle-dollar"
+              src={"/assets/icons/circle-dollar.svg"}
+              width={14}
+              height={12}
+            />
+            <span className="text-xxs lg:text-sm font-semibold lg:font-normal text-gray-950">{salary}</span>
+          </span>
+          <span className="flex items-center gap-2.5">
+            <Image
+              alt="users"
+              src={"/assets/icons/users.svg"}
+              width={18}
+              height={12}
+            />
+            <span className="text-xxs lg:text-sm font-semibold lg:font-normal text-gray-950">{salary}</span>
+          </span>
+          <span className="flex items-center gap-2.5">
+            <Image
+              alt="calendar-alt"
+              src={"/assets/icons/calendar-alt.svg"}
+              width={13}
+              height={12}
+            />
+            <span className="text-xxs lg:text-sm font-semibold lg:font-normal text-gray-950">{salary}</span>
           </span>
         </div>
-
-        <div className="flex items-center justify-between flex-1">
+        {/* <div className="flex items-center justify-between flex-1">
           <span className="flex items-center gap-2.5 font-sans text-10 lg:text-sm lg:leading-5 font-semibold leading-3">
             <CompanyDollar width={14} height={12} />
             <span className="text-10 lg:text-sm lg:leading-5">{salary}</span>
@@ -57,7 +98,7 @@ export default function JobCardDetails({
             <CompanyCalender width={13} height={12} />
             <span className="text-10">{date}</span>
           </span>
-        </div>
+        </div> */}
       </div>
     </>
   );
