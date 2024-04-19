61182f7b287bc8cd0e2f201852c1cb6a6165bb2a
bytegenius01 <git config --global user.email>
2024-04-20 01:49:24 +0500

    feat: separate store for contact form and also bind dta with inputs

--- src/forms/onBoarding/contact-form.tsx
+++ src/forms/onBoarding/contact-form.tsx
@@ -2,16 +2,25 @@ import CompanyGithubSecond from "@/components/svgs/company-github-second";
 import CompanyPhone from "@/components/svgs/company-phone";
 import CompanyTwitter from "@/components/svgs/company-twitter";
 import AppInput from "@/components/ui/app-input";
+import useOnBoardingContactData from "@/stores/on-boarding-contact-from-store";
 
 export default function ContactForm() {
+  const { onBoardingContactData, setOnBoardingContactData } =
+    useOnBoardingContactData();
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setOnBoardingContactData({ ...onBoardingContactData, [name]: value });
+  }
   return (
     <>
       <div className="flex flex-col gap-4 lg:gap-8">
         <AppInput
           variant={"primary"}
           name="phoneNumber"
+          value={onBoardingContactData.phoneNumber}
           type="text"
           placeholder="Phone Number"
+          onChange={handleChange}
           leadingIcon={<CompanyPhone width={16} height={16} />}
         />
         <h3 className="text-mute text-base lg:text-lg font-bold lg:font-semibold font-sans">
@@ -19,13 +28,17 @@ export default function ContactForm() {
         </h3>
         <AppInput
           variant={"primary"}
-          name="Linkedin"
+          name="linkedin"
+          value={onBoardingContactData.linkedin}
+          onChange={handleChange}
           type="text"
           placeholder="Linkedin"
         />
         <AppInput
           variant={"primary"}
-          name="X"
+          name="twitter"
+          value={onBoardingContactData.twitter}
+          onChange={handleChange}
           type="text"
           placeholder="X"
           leadingIcon={<CompanyTwitter width={16} height={16} />}
@@ -33,6 +46,8 @@ export default function ContactForm() {
         <AppInput
           variant={"primary"}
           name="github"
+          value={onBoardingContactData.github}
+          onChange={handleChange}
           type="text"
           placeholder="github"
           leadingIcon={<CompanyGithubSecond width={16} height={16} />}
