c7bb4ac396290516180d42d4c797f6d7b23d5dff
bytegenius01 <git config --global user.email>
2024-04-19 13:38:46 +0500

    fix:  added all register inputs in form component

--- src/forms/register-form/register-form.tsx
+++ src/forms/register-form/register-form.tsx
@@ -1,9 +1,18 @@
 import PageHeader from "@/_components/page-header";
+import CompanyMail from "@/components/svgs/coompany-mail";
+import DropDown from "@/_components/dropdown";
+import CompanyLock from "@/components/svgs/company-lock";
+import CompanyEye from "@/components/svgs/company-eye";
+import CompanyIcon from "@/components/svgs/company-icon";
+import CompanyProfileOne from "@/components/svgs/company-profile-one";
+import CompanyCommunity from "@/components/svgs/company-community";
+import CompanyGlobe from "@/components/svgs/company-globe";
+import CompanyCity from "@/components/svgs/company-city";
+import CompanyDictionary from "@/components/svgs/company-dictionary";
+import AppInput from "@/components/ui/app-input";
 import VectorText from "@/_components/vector-text";
 import AppsAuth from "@/_components/apps-auth";
 import TabNavigator from "@/_components/tab-navigator";
-import RegisterCandidateInputs from "./register-candidate-inputs";
-import RegisterCompanyInputs from "./register-company-inputs";
 import Button from "@/components/ui/app-button";
 import CompanyArrow from "@/components/svgs/company-arrow";
 import LoginRegisterToggler from "@/_components/login-register-toggler";
@@ -58,24 +67,118 @@ export default function RegisterForm({
       <VectorText text="or" />
       <TabNavigator tabs={Tabs} />
       {opened === "Company" ? (
-        <RegisterCompanyInputs
-          handleChange={handleChange}
-          employees={employOptions}
-          selectEmploy={employs}
-          setSelectEmploy={setEmploys}
-          countries={countries}
-          selectCountry={country}
-          setSelectCountry={setCountry}
-          languages={languages}
-          selectLanguage={language}
-          setSelectLanguage={setLanguage}
-          registerFormData={registerFormData}
-        />
+        <>
+          <AppInput
+            variant={"primary"}
+            placeholder="Company"
+            name="company"
+            type="text"
+            leadingIcon={<CompanyIcon />}
+            onChange={handleChange}
+            value={registerFormData.company}
+          />
+          <AppInput
+            variant={"primary"}
+            placeholder="Contact Name"
+            name="contactName"
+            type="text"
+            leadingIcon={<CompanyProfileOne />}
+            onChange={handleChange}
+            value={registerFormData.contactName}
+          />
+          <AppInput
+            variant={"primary"}
+            placeholder="Work Mail"
+            name="workMail"
+            type="text"
+            leadingIcon={<CompanyMail width={14} height={11} />}
+            onChange={handleChange}
+            value={registerFormData.workMail}
+          />
+          <AppInput
+            variant={"primary"}
+            placeholder="Phone Number"
+            name="phoneNumber"
+            type="text"
+            leadingIcon={<CompanyArrow width={15} height={15} />}
+            onChange={handleChange}
+            value={registerFormData.workMail}
+          />
+
+          <div className="px-3 py-2 flex items-center gap-2.5 bg-white w-full border-gray-300">
+            <CompanyCommunity />
+            <DropDown
+              options={employOptions}
+              select={employs}
+              setSelect={setEmploys}
+            />
+          </div>
+          <div className="px-3 py-2 flex items-center gap-2.5 bg-white w-full border-gray-300">
+            <CompanyGlobe />
+            <DropDown
+              options={countries}
+              select={country}
+              setSelect={setCountry}
+            />
+          </div>
+          <AppInput
+            placeholder="City"
+            type="text"
+            name="city"
+            leadingIcon={<CompanyCity />}
+            onChange={handleChange}
+            value={registerFormData.city}
+          />
+          <div className="px-3 py-2 flex items-center gap-2.5 bg-white w-full border-gray-300">
+            <CompanyDictionary />
+            <DropDown
+              options={languages}
+              select={language}
+              setSelect={setLanguage}
+            />
+          </div>
+        </>
       ) : (
-        <RegisterCandidateInputs
-          handleChange={handleChange}
-          registerFormData={registerFormData}
-        />
+        <>
+          <AppInput
+            variant={"primary"}
+            type="text"
+            name="username"
+            value={registerFormData.username}
+            onChange={handleChange}
+            placeholder="Username"
+            leadingIcon={<CompanyProfileOne />}
+          />
+          <AppInput
+            variant={"primary"}
+            type="email"
+            name="email"
+            value={registerFormData.email}
+            onChange={handleChange}
+            placeholder="Email"
+            leadingIcon={<CompanyMail width={14} height={11} />}
+          />
+          <AppInput
+            variant={"primary"}
+            name="passwordOne"
+            type="password"
+            value={registerFormData.passwordOne}
+            onChange={handleChange}
+            placeholder="Password"
+            leadingIcon={<CompanyLock width={14} height={15} />}
+            trailingIcon={<CompanyEye width={16} height={13} />}
+          />
+          <AppInput
+            variant={"primary"}
+            name="passwordTwo"
+            type="password"
+            value={registerFormData.passwordTwo}
+            onChange={handleChange}
+            placeholder="Password"
+            leadingIcon={<CompanyLock width={14} height={15} />}
+            trailingIcon={<CompanyEye width={16} height={13} />}
+          />
+        </>
       )}
       <Button
         text="Continue"
