ccb4eed943f3c2210ebcce3ee1093b36fe1312bc
bytegenius01 <git config --global user.email>
2024-04-19 20:04:34 +0500

    fix: password fields issue in validation

--- src/types/schemas/register-candidate-form-schema.ts
+++ src/types/schemas/register-candidate-form-schema.ts
@@ -1,17 +1,18 @@
 import { z } from "zod";
-export const registerCandidateFormSchema = z.object({
-  username: z
-    .string()
-    .min(2, { message: "Username must be at least 2 characters long" }),
-  email: z.string().email({ message: "Email must be valid" }),
-  passwordOne: z
-    .string()
-    .min(6, { message: "Password must be at least 6 characters long" }),
-  passwordTwo: z
-    .string()
-    .min(6, { message: "Password must be at least 6 characters long" })
-    .refine((val: any, ctx: any) => val === ctx.passwordOne, {
-      message: "Passwords do not match",
-      path: ["passwordTwo"],
-    }),
-});
+export const registerCandidateFormSchema = z
+  .object({
+    username: z
+      .string()
+      .min(2, { message: "Username must be at least 2 characters long" }),
+    email: z.string().email({ message: "Email must be valid" }),
+    passwordOne: z
+      .string()
+      .min(6, { message: "Password must be at least 6 characters long" }),
+    passwordTwo: z
+      .string()
+      .min(6, { message: "Password must be at least 6 characters long" }),
+  })
+  .refine((data) => data.passwordOne === data.passwordTwo, {
+    message: "Passwords don't match",
+    path: ["confirm"],
+  });
