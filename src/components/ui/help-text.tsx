a4a72b940d4b897ddb5e9e989ff750d664698983
bytegenius01 <git config --global user.email>
2024-04-22 21:30:37 +0500

    fix: color class prop

--- src/components/ui/help-text.tsx
+++ src/components/ui/help-text.tsx
@@ -1,7 +1,11 @@
 type HelpTextProps = {
   text: string;
-  className?: string;
+  colorClass?: string;
 };
-export default function HelpText({ text, className }: HelpTextProps) {
-  return <p className={`text-xs text-red-500 gap-1 ${className}`}>{text}</p>;
+export default function HelpText({ text, colorClass }: HelpTextProps) {
+  return (
+    <p className={`text-xs gap-1 ${colorClass ? "" : "text-red-500"}`}>
+      {text}
+    </p>
+  );
 }
