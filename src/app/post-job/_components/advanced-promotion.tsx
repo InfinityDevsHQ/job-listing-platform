ec3ffdcb0b893ccffa10fa49282c284af1d664e6
bytegenius01 <git config --global user.email>
2024-04-23 13:57:18 +0500

    feat: advanced promotion section

---
+++ src/app/post-job/_components/advanced-promotion.tsx
@@ -0,0 +1,9 @@
+export default function AdvancedPromotion() {
+  return (
+    <section>
+      <h2 className="text-base text-gray-700 text-center font-bold mb-4">
+        Accelerate your recruitment through advanced promotion
+      </h2>
+    </section>
+  );
+}
--- src/app/post-job/page.tsx
+++ src/app/post-job/page.tsx
@@ -1,9 +1,11 @@
 import PostJobForm from "@/forms/post-jos-form/post-job-form";
+import AdvancedPromotion from "./_components/advanced-promotion";
 
 export default function PostJob() {
   return (
     <div>
       <PostJobForm />
+      <AdvancedPromotion />
     </div>
   );
 }
