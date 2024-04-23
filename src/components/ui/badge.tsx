2e99b47d82bcb0fea53e142ce4fd7527b810c74a
Saad Gulzar <isaadgulzar@gmail.com>
2024-04-23 20:24:22 +0500

    refactor: improve code

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,8 +1,16 @@
 import { BadgeProps } from "@/types/types";
+import { cn } from "@/lib/utils";
 
 const Badge = ({ text, color, bgColor }: BadgeProps) => {
 	return (
-		<span className="rounded-md text-sm font-bold text-neutral-500 border border-neutral-200 bg-gray-100 py-1 px-2">
+		<span
+			className={cn(
+				"rounded-md text-sm font-bold py-1 px-2 border border-neutral-200 bg-gray-100 text-neutral-500",
+				{
+					"border-transparent": bgColor,
+				}
+			)}
+		>
 			{text}
 		</span>
 	);
