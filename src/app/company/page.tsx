c41f33131c64cefec64e13465fe60a3377ab7079
bytegenius01 <git config --global user.email>
2024-04-23 19:47:39 +0500

    feat: 3cols grid system

--- src/app/company/page.tsx
+++ src/app/company/page.tsx
@@ -83,60 +83,68 @@ export default function Company() {
           className="absolute top-96 left-8 rounded-full"
         />
       </header>
-      {/* NAMES */}
-      <div className="flex lg:gap-8 mt-14">
-        <h4 className="text-black font-sans lg:text-4xl font-bold">
-          Company Name
-        </h4>
-        <Button text="Follow" />
-        <Button text="Get Emails About New Jobs" value={"primary"} />
-      </div>
-      {/* BIO */}
-      <p className="font-sans lg:text-xl text-gray-500">Silicon Valley</p>
-      <span className="border border-gray-200" />
-      {/* ABOUT */}
-      <>
-        <SectionHeading text="About" />
-        <p className="font-sans text-base text-gray-500">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere
-          fugiat in totam nisi! Tempore reprehenderit quibusdam quasi voluptas.
-          Laboriosam nesciunt libero quod commodi facilis rerum recusandae
-          veritatis. Placeat, quos dolorum?
-        </p>
-        <p className="font-sans text-base text-gray-500">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere
-          fugiat in totam nisi! Tempore reprehenderit quibusdam quasi voluptas.
-          Laboriosam nesciunt libero quod commodi facilis rerum recusandae
-          veritatis. Placeat, quos dolorum? Lorem ipsum dolor, sit amet
-          consectetur adipisicing elit. Commodi minima incidunt inventore quod
-          quaerat provident numquam voluptatibus maxime velit atque quis,
-          impedit tempore autem dolores? Officiis qui quos distinctio sequi!
-        </p>
-      </>
-      {/* Technologies */}
-      <SectionHeading text="What Technologies We Use" />
-      {/* Team */}
-      <SectionHeading text="Our Team" />
-      <div className="flex flex-col justify-center items-center lg:gap-2 max-w-max">
-        <Image
-          className="rounded-md"
-          src={"/assets/company/team.png"}
-          alt="This"
-          width={150}
-          height={150}
-        />
-        <h5 className="text-gray-500 lg:text-base font-semibold">John Doe</h5>
-        <p className="text-xs text-gray-500 font-sans">CEO</p>
-        <div className="flex items-center gap-2.5">
-          <CompanyLinkedin black width={11} height={12} />
-          <CompanyInsta width={11} height={12} />
+      <div className="grid grid-cols-3">
+        <div className="col-span-2 flex flex-col gap-4 lg:gap-8">
+          {/* NAMES */}
+          <div className="flex lg:gap-8 mt-14">
+            <h4 className="text-black font-sans lg:text-4xl font-bold">
+              Company Name
+            </h4>
+            <Button text="Follow" />
+            <Button text="Get Emails About New Jobs" variant={"primary"} />
+          </div>
+          {/* BIO */}
+          <p className="font-sans lg:text-xl text-gray-500">Silicon Valley</p>
+          <span className="border border-gray-200" />
+          {/* ABOUT */}
+          <>
+            <SectionHeading text="About" />
+            <p className="font-sans text-base text-gray-500">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere
+              fugiat in totam nisi! Tempore reprehenderit quibusdam quasi
+              voluptas. Laboriosam nesciunt libero quod commodi facilis rerum
+              recusandae veritatis. Placeat, quos dolorum?
+            </p>
+            <p className="font-sans text-base text-gray-500">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere
+              fugiat in totam nisi! Tempore reprehenderit quibusdam quasi
+              voluptas. Laboriosam nesciunt libero quod commodi facilis rerum
+              recusandae veritatis. Placeat, quos dolorum? Lorem ipsum dolor,
+              sit amet consectetur adipisicing elit. Commodi minima incidunt
+              inventore quod quaerat provident numquam voluptatibus maxime velit
+              atque quis, impedit tempore autem dolores? Officiis qui quos
+              distinctio sequi!
+            </p>
+          </>
+          {/* Technologies */}
+          <SectionHeading text="What Technologies We Use" />
+          {/* Team */}
+          <SectionHeading text="Our Team" />
+          <div className="flex flex-col justify-center items-center lg:gap-2 max-w-max">
+            <Image
+              className="rounded-md"
+              src={"/assets/company/team.png"}
+              alt="This"
+              width={150}
+              height={150}
+            />
+            <h5 className="text-gray-500 lg:text-base font-semibold">
+              John Doe
+            </h5>
+            <p className="text-xs text-gray-500 font-sans">CEO</p>
+            <div className="flex items-center gap-2.5">
+              <CompanyLinkedin black width={11} height={12} />
+              <CompanyInsta width={11} height={12} />
+            </div>
+          </div>
+          {/* Jobs Opportunities */}
+          <SectionHeading text="Job Opportunities" />
+          {jobs?.map((job, index) => (
+            <JobCard key={index} job={job} />
+          ))}
         </div>
+        <div></div>
       </div>
-      {/* Jobs Opportunities */}
-      <SectionHeading text="Job Opportunities" />
-      {jobs?.map((job, index) => (
-        <JobCard key={index} job={job} />
-      ))}
     </div>
   );
 }
