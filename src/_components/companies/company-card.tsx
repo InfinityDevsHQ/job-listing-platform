edebb155d8fdd3ee4605ab3983f0f4617c891b9c
Saad Gulzar <isaadgulzar@gmail.com>
2024-04-24 18:40:09 +0500

    fix: fix company card UI

--- src/_components/companies/company-card.tsx
+++ src/_components/companies/company-card.tsx
@@ -1,50 +1,39 @@
-import Image from "next/image";
+import Image from 'next/image';
 
-import CompanyArrow from "@/components/svgs/company-arrow";
-import CompanyPhone from "@/components/svgs/company-phone";
-import Button from "@/components/ui/button";
+import Button from '@/components/ui/button';
 
-import { CompanyCardProps } from "@/types/types";
+import { CompanyCardProps } from '@/types/types';
 
-const CompanyCard = ({
-	companyName,
-	companyLogo,
-	companyDescription,
-}: CompanyCardProps) => {
-	return (
-		<div className="flex flex-col gap-4 lg:gap-8 p-4 lg:p-8 bg-white rounded-md border border-gray-200">
-			<div className="flex items-center gap-5">
-				<div className="flex items-center justify-center h-16 w-16 rounded-full bg-neutral-50">
-					<Image
-						src={companyLogo}
-						alt={`${companyName} logo`}
-						width={30}
-						height={30}
-						className="lg:hidden"
-					/>
-					<Image
-						src={companyLogo}
-						alt={`${companyName} logo`}
-						width={44}
-						height={44}
-						className="hidden lg:block"
-					/>
-				</div>
-				<h3 className="font-bold text-3xl lg:text-4xl text-neutral-950">
-					{companyName}
-				</h3>
-			</div>
-			<p className="text-sm lg:text-base text-neutral-500">
-				{companyDescription}
-			</p>
-			<div className="flex justify-end">
-				<Button
-					text="Load More"
-					variant="primary"
-				/>
-			</div>
-		</div>
-	);
+const CompanyCard = ({ companyName, companyLogo, companyDescription }: CompanyCardProps) => {
+  return (
+    <div className="flex flex-col gap-4 rounded-md border border-gray-200 bg-white p-4 lg:gap-8 lg:p-8">
+      <div className="flex items-center gap-2.5 lg:gap-5">
+        <div className="flex h-10 w-10 items-center justify-center rounded-full bg-neutral-50 lg:h-16 lg:w-16">
+          <Image
+            src={companyLogo}
+            alt={`${companyName} logo`}
+            width={30}
+            height={30}
+            className="lg:hidden"
+          />
+          <Image
+            src={companyLogo}
+            alt={`${companyName} logo`}
+            width={44}
+            height={44}
+            className="hidden lg:block"
+          />
+        </div>
+        <h3 className="text-3xl font-bold text-neutral-950 lg:text-4xl">{companyName}</h3>
+      </div>
+      <p className="lg:min-w-none min-w-72 text-sm text-neutral-500 lg:text-base">
+        {companyDescription}
+      </p>
+      <div className="flex justify-end">
+        <Button text="Load More" variant="primary" />
+      </div>
+    </div>
+  );
 };
 
 export default CompanyCard;
