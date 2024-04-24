e492945671d2c66cb2aaca7181fc48a381b21f22
Saad Gulzar <isaadgulzar@gmail.com>
2024-04-24 17:06:39 +0500

    fix: fix header ui bugs

--- src/_components/header/app-navigation.tsx
+++ src/_components/header/app-navigation.tsx
@@ -1,9 +1,8 @@
-"use client"
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
-import { usePathname } from "next/navigation";
-import Link from "next/link";
-
-import MobileNav from "./mobile-nav";
+import MobileNav from './mobile-nav';
 
 type Tabs = {
   text: string;
@@ -11,26 +10,26 @@ type Tabs = {
 };
 
 const AppNavigation = () => {
-  const pathname  = usePathname();
+  const pathname = usePathname();
 
   const tabs: Tabs[] = [
-    { text: "Home", href: "/" },
-    { text: "About", href: "/about" },
-    { text: "Contact Us", href: "/contact" },
-    { text: "For Companies", href: "/companies" },
+    // { text: 'Home', href: '/' },
+    // { text: 'About', href: '/about' },
+    // { text: 'Contact Us', href: '/contact' },
+    { text: 'For Companies', href: '/companies' },
   ];
 
   return (
     <>
-      <nav className="hidden lg:block">
+      <nav className="hidden flex-shrink-0 lg:block">
         <ol className="flex gap-8">
           {tabs.map((link, index) => (
             <li key={index}>
-              <Link 
+              <Link
                 href={link.href}
-                className={`text-xl font-semibold ${pathname === link.href ? 'text-primary-900' : 'text-neutral-500'}`}
-                >
-                  {link.text}
+                className={`text-xl ${pathname === link.href ? 'text-primary-900' : 'text-neutral-500'}`}
+              >
+                {link.text}
               </Link>
             </li>
           ))}
--- src/_components/header/header.tsx
+++ src/_components/header/header.tsx
@@ -1,52 +1,61 @@
-"use client"
+'use client';
 
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
-import { useRouter } from "next/navigation"
+import { useRouter } from 'next/navigation';
 
-import AppNavigation from "./app-navigation";
-import Button from "@/components/ui/button";
+import Button from '@/components/ui/button';
+import AppNavigation from './app-navigation';
 
-import LockIcon from "@/components/svgs/lock";
-import ArrowRightIcon from "@/components/svgs/arrow-right";
+import ArrowRightIcon from '@/components/svgs/arrow-right';
+import LockIcon from '@/components/svgs/lock';
+import { ChevronsUpDown } from 'lucide-react';
 
 export default function Header() {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <>
-      <header className="flex items-center justify-between w-full px-4 py-2 lg:px-16 lg:py-8 bg-white">
-        <Link href={"/"}>
+      <header className="flex w-full items-center justify-between bg-white px-4 py-2 lg:px-16 lg:py-8">
+        <Link href={'/'}>
           <Image
             alt="logo"
-            src={"/assets/images/common/logo_clickJob_primary.svg"}
+            src={'/assets/images/common/logo_clickJob_primary.svg'}
             width={226}
             height={50}
             className="hidden lg:block"
           />
           <Image
             alt="logo"
-            src={"/assets/images/common/logo_clickJob_primary.svg"}
+            src={'/assets/images/common/logo_clickJob_primary.svg'}
             width={118}
             height={30}
             className="block lg:hidden"
           />
         </Link>
-        <AppNavigation />
-        <span className="hidden lg:flex gap-2">
-          <Button
-            text="Login"
-            variant={"outline-primary"}
-            trailingIcon={<LockIcon />}
-            onClick={() => router.push('/login')}
-          />
-          <Button
-            text="Register"
-            trailingIcon={<ArrowRightIcon />}
-            variant={"primary"}
-            onClick={() => router.push('/register')}
-          />
-        </span>
+        <div className="flex items-center gap-4">
+          <AppNavigation />
+
+          <div className="hidden gap-4 lg:flex">
+            <Button
+              variant="outline-primary"
+              text="Country"
+              trailingIcon={<ChevronsUpDown className="h-4 w-4" />}
+            />
+            <Button
+              text="Login"
+              variant={'primary'}
+              trailingIcon={<LockIcon />}
+              onClick={() => router.push('/login')}
+            />
+            <Button
+              text="Register"
+              trailingIcon={<ArrowRightIcon />}
+              variant={'primary'}
+              onClick={() => router.push('/register')}
+            />
+          </div>
+        </div>
       </header>
     </>
   );
