0fc8f10b5c02eddd6ade6dfa26f8317867809a2b
bytegebius01 <bytegenius@infinitydevs.io>
2024-04-24 08:09:34 -0700

    feat: missing panel on company page

--- src/app/company/page.tsx
+++ src/app/company/page.tsx
@@ -2,7 +2,9 @@
 import JobCard from '@/_components/jobs/_components/job-card';
 import CompanyInsta from '@/components/svgs/company-insta';
 import CompanyLinkedin from '@/components/svgs/company-linedin';
+import CompanyLocation from '@/components/svgs/company-location';
 import Button from '@/components/ui/button';
+import Input from '@/components/ui/input';
 import useJobListingsData from '@/stores/job-listings-store';
 import Image from 'next/image';
 import SectionHeading from './section-heading';
@@ -23,7 +25,7 @@ export default function Company() {
           className="absolute left-8 top-96 rounded-full"
         />
       </header>
-      <div className="grid grid-cols-3">
+      <div className="grid grid-cols-3 lg:gap-8">
         <div className="col-span-2 flex flex-col gap-4 lg:gap-8">
           {/* NAMES */}
           <div className="mt-14 flex lg:gap-8">
@@ -74,7 +76,54 @@ export default function Company() {
           <SectionHeading text="Job Opportunities" />
           {allJobs?.map((job, index) => <JobCard key={index} job={job} />)}
         </div>
-        <div></div>
+        <div className="flex flex-col gap-8">
+          <div className="flex flex-col gap-8 rounded-md border border-gray-200 p-8">
+            <h6 className="font-bold text-gray-700 lg:text-xl">Job Openings</h6>
+            <p className="font-bold text-gray-700 lg:text-4xl">200+</p>
+            <h5 className="font-bold text-gray-700 lg:text-xl">Contacts</h5>
+            <div className="hidden items-center gap-2 lg:flex">
+              <span className="flex items-center justify-center rounded-full bg-primary-900 p-2.5 text-white">
+                <CompanyLocation width={11} height={12} />
+              </span>
+              <div className="flex flex-col">
+                <p className="font-bold text-gray-700 lg:text-base">Location</p>
+                <p className="text-base text-gray-500">Berlin, Germany</p>
+              </div>
+            </div>
+            <div className="hidden items-center gap-2 lg:flex">
+              <span className="flex items-center justify-center rounded-full bg-primary-900 p-2.5 text-white">
+                <CompanyLocation width={11} height={12} />
+              </span>
+              <div className="flex flex-col">
+                <p className="font-bold text-gray-700 lg:text-base">Website</p>
+                <p className="text-base text-gray-500">123 Maple Street, Springfield, 62704 USA</p>
+              </div>
+            </div>
+            <div className="hidden items-center gap-2 lg:flex">
+              <span className="flex items-center justify-center rounded-full bg-primary-900 p-2.5 text-white">
+                <CompanyLocation width={11} height={12} />
+              </span>
+              <div className="flex flex-col">
+                <p className="font-bold text-gray-700 lg:text-base">Email</p>
+                <p className="text-base text-gray-500">company@company.com</p>
+              </div>
+            </div>
+            <h5 className="font-bold text-gray-700 lg:text-xl">News from the company</h5>
+            <h5 className="font-bold text-gray-700 lg:text-xl">What does it look like for us?</h5>
+            <div className="flex"></div>
+          </div>
+          <div className="flex flex-col gap-8 rounded-md border border-gray-200 p-8">
+            <h5 className="font-bold text-gray-700 lg:text-xl">
+              Did&apos;t choose from current offers?
+            </h5>
+            <p className="text-gray-500 lg:text-base">
+              Leave us your email and we will keep you informed about new offers from Company.
+              According to our rules.
+            </p>
+            <Input placeholder="Email" type="email" />
+            <Button text="Subscribe" variant="primary" className="self-end" />
+          </div>
+        </div>
       </div>
     </div>
   );
