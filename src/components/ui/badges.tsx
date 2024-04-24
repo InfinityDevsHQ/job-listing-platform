9725d4537226b93786e9ab85fda3b22244c95c28
Saad Gulzar <isaadgulzar@gmail.com>
2024-04-24 18:39:38 +0500

    fix: fix job card badges UI

--- src/components/ui/badges.tsx
+++ src/components/ui/badges.tsx
@@ -1,24 +1,19 @@
-import { BadgeProps } from "@/types/types";
+import { BadgeProps } from '@/types/types';
 
-import Badge from "@/components/ui/badge";
+import Badge from '@/components/ui/badge';
 
 type BadgesProps = {
-	badges?: BadgeProps[];
+  badges?: BadgeProps[];
 };
 
 const Badges = ({ badges }: BadgesProps) => {
-	return (
-		<div className="flex items-center gap-2.5">
-			{badges?.map((badge, index) => (
-				<Badge
-					key={index}
-					text={badge?.text}
-					color={badge?.color}
-					bgColor={badge?.bgColor}
-				/>
-			))}
-		</div>
-	);
+  return (
+    <div className="flex w-full flex-wrap items-center gap-2.5">
+      {badges?.map((badge, index) => (
+        <Badge key={index} text={badge?.text} color={badge?.color} bgColor={badge?.bgColor} />
+      ))}
+    </div>
+  );
 };
 
 export default Badges;
