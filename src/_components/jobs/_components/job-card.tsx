993ff5645afc4029915e2f37e33e371896e11931
Saad Gulzar <isaadgulzar@gmail.com>
2024-04-24 19:21:58 +0500

    feat: click job card to navigate to details

--- src/_components/jobs/_components/job-card.tsx
+++ src/_components/jobs/_components/job-card.tsx
@@ -2,26 +2,33 @@ import JobCardDetails from '@/_components/jobs/_components/job-card-details';
 import JobCardHeader from '@/_components/jobs/_components/job-card-header';
 import Badges from '@/components/ui/badges';
 import { JobProps } from '@/types/types';
+import Link from 'next/link';
 import { useState } from 'react';
 
 export default function JobCard({ job }: JobProps) {
   const [isOpened, setIsOpened] = useState(false);
 
   return (
-    <div
-      onClick={() => setIsOpened(!isOpened)}
-      className="flex cursor-pointer select-none flex-col gap-4 overflow-hidden rounded-md border border-gray-200 bg-white p-4 duration-500 animate-in fade-in lg:gap-8 lg:p-8"
-    >
-      <JobCardHeader job={job} />
-      <JobCardDetails job={job} />
+    <div className="flex flex-col gap-4 overflow-hidden rounded-md border border-gray-200 bg-white p-4 duration-500 animate-in fade-in lg:gap-8 lg:p-8">
+      <div
+        className="flex cursor-pointer select-none flex-col gap-4 lg:gap-8"
+        onClick={() => setIsOpened(!isOpened)}
+      >
+        <JobCardHeader job={job} />
+        <JobCardDetails job={job} />
+      </div>
       {isOpened && (
-        <div className="flex flex-col gap-4 duration-500 animate-in fade-in fade-out lg:gap-8">
+        <Link
+          className="flex cursor-default flex-col gap-4 duration-500 animate-in fade-in fade-out lg:gap-8"
+          onClick={() => setIsOpened(!isOpened)}
+          href={`/jobs/${job?.id}`}
+        >
           {job?.description && (
             <p className="text-xs text-neutral-600 lg:text-sm">{job?.description}</p>
           )}
           <span className="border-b border-gray-200" />
           <Badges badges={job?.skill_tags?.map((tag) => ({ text: tag }))} />
-        </div>
+        </Link>
       )}
     </div>
   );
