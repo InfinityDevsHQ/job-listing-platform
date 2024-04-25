5ede3413eb81e98a930f6a08f3fe079c0569ea97
bytegebius01 <bytegenius@infinitydevs.io>
2024-04-25 04:05:13 -0700

    feat: candidates page

--- src/app/(auth)/candidates/_components/candidate-card.tsx
+++ src/app/(auth)/candidates/_components/candidate-card.tsx
@@ -4,16 +4,9 @@ import CompanyLocation from '@/components/svgs/company-location';
 import Button from '@/components/ui/button';
 import { ArrowRight } from 'lucide-react';
 import Image from 'next/image';
-type CandidateData = {
-  name: string;
-  candidateImgUrl: string;
-  profession: string;
-  location: string;
-  about: string;
-  skillSet: string[];
-};
+
 type CandidateCardProps = {
-  candidateData: CandidateData[];
+  candidateData: CandidateCardData;
 };
 export default function CandidateCard({ candidateData }: CandidateCardProps) {
   return (
@@ -22,37 +15,37 @@ export default function CandidateCard({ candidateData }: CandidateCardProps) {
         <header className="flex gap-4">
           <div className="flex items-center justify-center">
             <Image
-              src="/assets/"
-              alt="ok"
+              src={candidateData.candidateImgUrl}
+              alt={`${candidateData.name} Profile Picture`}
               width={75}
               height={75}
-              className="h-auto w-auto rounded-full"
+              className="h-auto w-auto flex-1 rounded-full lg:self-stretch"
             />
           </div>
           <div className="flex flex-col">
-            <h5 className="text-lg font-bold text-gray-950">John Doe</h5>
-            <h6 className="text-sm text-gray-500">Software Engineer</h6>
+            <h5 className="text-lg font-bold text-gray-950">{candidateData.name}</h5>
+            <h6 className="text-sm text-gray-500">{candidateData.profession}</h6>
             <div className="flex items-center gap-2 text-gray-950">
               <CompanyLocation width={11} height={12} />
-              <p>NY, USA</p>
+              <p>{candidateData.location}</p>
             </div>
           </div>
         </header>
         <div className="flex items-center gap-4">
-          <Button text="View Profile" leadingIcon={<CompanyEye width={16} height={16} />} />
-          <Button text="Resume" leadingIcon={<CompanyEye width={16} height={16} />} />
+          <Button
+            text="View Profile"
+            leadingIcon={<CompanyEye width={16} height={16} />}
+            size="full"
+          />
+          <Button text="Resume" leadingIcon={<CompanyEye width={16} height={16} />} size="full" />
         </div>
         <div className="flex flex-col gap-4">
           <h4 className="text-base font-bold text-gray-700">About</h4>
-          <p className="text-base text-gray-500">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa excepturi tempora
-            voluptas ducimus voluptate, tenetur corrupti eaque qui, ratione tempore aut veritatis
-            consectetur, exercitationem quidem explicabo labore. Atque, quos nulla!
-          </p>
+          <p className="text-base text-gray-500">{candidateData.about}</p>
         </div>
         <div className="flex items-center justify-between">
           <Button text="" leadingIcon={<ArrowRight />} />
-          <Tags python sql django />
+          <Tags django sql python />
           <Button text="" leadingIcon={<ArrowRight />} />
         </div>
         <Button text="Add to Cart" size="full" variant="primary" />
