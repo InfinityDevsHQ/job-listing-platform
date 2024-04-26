a39521321e9914f1287a8591df0326f73bbc688c
bytegebius01 <bytegenius@infinitydevs.io>
2024-04-26 12:14:10 -0700

    feat: fetching data for all countries from api

--- src/api/countries/route.ts
+++ src/api/countries/route.ts
@@ -1,5 +1,9 @@
 export async function GET() {
-  const res = await fetch('http://devel.clickjob.ai:8001//api/v1/platform/country');
+  const res = await fetch('http://devel.clickjob.ai:8000/api/v1/platform/country', {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
   const data = await res.json();
   return Response.json({ data });
 }
--- src/forms/onBoarding/location-form.tsx
+++ src/forms/onBoarding/location-form.tsx
@@ -1,42 +1,44 @@
-"use client";
-import Input from "@/components/ui/input";
-import { useQueryParams } from "@/hooks/useQueryParams";
-import CompanyGlobe from "@/components/svgs/company-globe";
-import CompanyEye from "@/components/svgs/company-eye";
-import DropDown from "@/_components/dropdown";
-import CompanyDictionary from "@/components/svgs/company-dictionary";
-import { useState } from "react";
-import { locationFormSchema } from "@/types/schemas/location-form-schema";
-import CompanyCity from "@/components/svgs/company-city";
-import useLocationFormData from "@/stores/location-form-store";
-import CompanyClock from "@/components/svgs/company-clock";
-import { ZodIssue } from "zod";
-import Pagination from "@/components/ui/pagination";
+'use client';
+import DropDown from '@/_components/dropdown';
+import CompanyCity from '@/components/svgs/company-city';
+import CompanyClock from '@/components/svgs/company-clock';
+import CompanyDictionary from '@/components/svgs/company-dictionary';
+import CompanyEye from '@/components/svgs/company-eye';
+import CompanyGlobe from '@/components/svgs/company-globe';
+import Input from '@/components/ui/input';
+import Pagination from '@/components/ui/pagination';
+import { useQueryParams } from '@/hooks/useQueryParams';
+import useCountriesData from '@/stores/get-countries-data';
+import useLocationFormData from '@/stores/location-form-store';
+import { locationFormSchema } from '@/types/schemas/location-form-schema';
+import { useState } from 'react';
+import { ZodIssue } from 'zod';
 const Languages = [
   {
-    value: "en",
-    text: "English",
+    value: 'en',
+    text: 'English',
   },
   {
-    value: "ur",
-    text: "Urdu",
+    value: 'ur',
+    text: 'Urdu',
   },
 ];
-const TimeZones = [{ value: "au", text: "Australia" }];
-const Cities = [{ value: "sidney", text: "Sidney, Australia" }];
+const Cities = [{ value: 'sidney', text: 'Sidney, Australia' }];
 export default function LocationForm() {
   const [preferLanguage, setPreferLanguage] = useState(Languages[0].value);
-  const [timeZone, setTimeZone] = useState(TimeZones[0].value);
+  const { allCountries } = useCountriesData();
+  console.log(allCountries);
+  const [timeZone, setTimeZone] = useState();
   const [errors, setErrors] = useState({
-    country: "",
-    password: "",
+    country: '',
+    password: '',
   });
   const [city, setCity] = useState(Cities[0].value);
   const { locationFormData, setLocationFormData } = useLocationFormData();
   const addQueryParams = useQueryParams();
   function handleChange(e) {
     const { name, value } = e.target;
-    setErrors({ ...errors, [name]: "" });
+    setErrors({ ...errors, [name]: '' });
     setLocationFormData({
       ...locationFormData,
       [name]: value,
@@ -49,10 +51,10 @@ export default function LocationForm() {
     e.preventDefault();
     const validate = locationFormSchema.safeParse(locationFormData);
     if (validate.success) {
-      console.log("DatA Validated", validate.data);
-      addQueryParams("step", "upload-cv");
+      console.log('DatA Validated', validate.data);
+      addQueryParams('step', 'upload-cv');
     } else {
-      console.warn("Invalid data type", validate.error.errors);
+      console.warn('Invalid data type', validate.error.errors);
       const validationErrors = validate.error.errors;
       const formattedErrors: { [key: string]: string } = {};
       validationErrors.forEach((error: ZodIssue) => {
@@ -79,7 +81,7 @@ export default function LocationForm() {
         leadingIcon={<CompanyClock width={16} height={16} />}
       />
       <Input
-        variant={"primary"}
+        variant={'primary'}
         name="country"
         value={locationFormData.country}
         helpText={errors.country}
@@ -88,14 +90,9 @@ export default function LocationForm() {
         placeholder="Country"
         leadingIcon={<CompanyGlobe />}
       />
-      <DropDown
-        options={Cities}
-        select={city}
-        setSelect={setCity}
-        leadingIcon={<CompanyCity />}
-      />
+      <DropDown options={Cities} select={city} setSelect={setCity} leadingIcon={<CompanyCity />} />
       <Input
-        variant={"primary"}
+        variant={'primary'}
         name="password"
         type="password"
         value={locationFormData.password}
