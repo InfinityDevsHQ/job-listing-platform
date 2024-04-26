b31b09b8fe8dbf209d04917de456710548d6c7a9
bytegebius01 <bytegenius@infinitydevs.io>
2024-04-26 07:10:40 -0700

    feat: responsive  message component

--- src/app/(auth)/inbox/_components/contact-card.tsx
+++ src/app/(auth)/inbox/_components/contact-card.tsx
@@ -5,9 +5,9 @@ const ContactCard = () => {
     <li className="flex flex-col gap-2.5 bg-white p-2.5 lg:gap-4 lg:p-4">
       <header className="flex items-center gap-5">
         <div className="flex items-center justify-center rounded-full">
-          <Image src={'/assets/inbox/avatar.png'} alt="Avatar Png" width={90} height={90} />
+          <Image src={'/assets/inbox/avatar.png'} alt="Avatar Png" width={50} height={40} />
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-1 flex-col">
           <h4 className="text-base font-bold text-gray-700">Jan Filips</h4>
           <p className="text-xs text-gray-500">Company</p>
         </div>
