e0908c4693e3b81d91105a01aac3569f5e144dbd
bytegebius01 <bytegenius@infinitydevs.io>
2024-04-26 11:17:59 -0700

    feat: chat section design

--- src/app/(auth)/inbox/_components/contact-card.tsx
+++ src/app/(auth)/inbox/_components/contact-card.tsx
@@ -1,32 +1,46 @@
 import Image from 'next/image';
-
-const ContactCard = () => {
+type ContactCardProps = {
+  senderImgUrl?: string;
+  senderName: string;
+  senderType: string;
+  notifications?: number;
+  date: string;
+  time: string;
+  message: string;
+};
+const ContactCard = ({
+  senderImgUrl = '/assets/inbox/avatar.png',
+  senderName = 'Avatar',
+  senderType,
+  notifications,
+  date,
+  time,
+  message,
+}: ContactCardProps) => {
   return (
     <li className="flex flex-col gap-2.5 bg-white p-2.5 lg:gap-4 lg:p-4">
       <header className="flex items-center gap-5">
         <div className="flex items-center justify-center rounded-full">
-          <Image src={'/assets/inbox/avatar.png'} alt="Avatar Png" width={50} height={40} />
+          <Image src={senderImgUrl} alt={`${senderName} Image`} width={50} height={40} />
         </div>
         <div className="flex flex-1 flex-col">
-          <h4 className="text-base font-bold text-gray-700">Jan Filips</h4>
-          <p className="text-xs text-gray-500">Company</p>
+          <h4 className="text-base font-bold text-gray-700">{senderName}</h4>
+          <p className="text-xs text-gray-500">{senderType}</p>
         </div>
         <div className="flex flex-col">
-          <span className="mb-auto flex h-4 w-4  items-center justify-center self-end rounded-full bg-red-500 text-center text-xs text-gray-50">
-            6
+          {notifications && (
+            <span className="mb-auto flex h-4 w-4  items-center justify-center self-end rounded-full bg-red-500 text-center text-xs text-gray-50">
+              {notifications}
+            </span>
+          )}
+          <span className="hidden text-xxs text-gray-500 lg:inline-block">
+            {date} {time}
           </span>
-          <span className="text-xxs text-gray-500 lg:hidden">24 August,2023 2:16 PM</span>
-          <span className="hidden self-end text-xxs text-gray-500 lg:inline-block">
-            24 August,2023
-          </span>
-          <span className="hidden self-end text-xxs text-gray-500 lg:inline-block">02:16 PM</span>
+          <span className="self-end text-xxs text-gray-500 lg:hidden">{date}</span>
+          <span className="self-end text-xxs text-gray-500">{time}</span>
         </div>
       </header>
-      <p className="h-12 overflow-hidden text-xs text-gray-500">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat exercitationem libero
-        possimus quasi mollitia repudiandae voluptatem esse cumque labore doloribus recusandae
-        ducimus, deserunt amet laboriosam, veniam reiciendis sed culpa totam!
-      </p>
+      <p className="h-12 overflow-hidden text-xs text-gray-500">{message}</p>
     </li>
   );
 };
--- src/app/(auth)/inbox/panels/_components/messages.tsx
+++ src/app/(auth)/inbox/panels/_components/messages.tsx
@@ -2,7 +2,7 @@ import Message from '../../_components/message';
 
 const Messages = () => {
   return (
-    <div className="flex flex-1 flex-col gap-5 bg-red-200 p-2.5 lg:gap-4 lg:p-8">
+    <div className="flex flex-1 flex-col gap-5 p-2.5 lg:gap-4 lg:p-8">
       <p className="mx-auto mt-auto text-xs font-bold text-mute-3">4 March, 2024</p>
       <Message
         type="incoming"
