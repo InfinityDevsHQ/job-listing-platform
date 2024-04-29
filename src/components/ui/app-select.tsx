8f8a24430aca832719baf7c92fb7c42807d7cf1e
bytegenious01 <bytegenius@infinitydevs.io>
2024-04-29 09:33:12 -0700

    refactor: converted into forward ref

--- src/components/ui/app-select.tsx
+++ src/components/ui/app-select.tsx
@@ -5,6 +5,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
+import React from 'react';
 
 interface OptionProp {
   value: string;
@@ -21,35 +22,47 @@ interface SelectProps {
   placeholder?: string;
 }
 
-export default function AppSelect({
-  options,
-  selectedValue,
-  onChange,
-  leadingIcon,
-  placeholder = 'Select',
-}: SelectProps) {
-  return (
-    <Select onValueChange={onChange} defaultValue={selectedValue?.value}>
-      <SelectTrigger className="w-full border border-gray-300 bg-white focus:border-primary-900 focus:ring-0">
-        <SelectValue
-          placeholder={
-            <p className="flex w-full items-center gap-2">
-              {leadingIcon}
-              <span>{placeholder}</span>
-            </p>
-          }
-        ></SelectValue>
-      </SelectTrigger>
-      <SelectContent className="bg-white">
-        {options?.map((option, index) => (
-          <SelectItem value={option?.value} key={index}>
-            <p className="flex items-center gap-2">
-              {option?.icon}
-              {option?.label}
-            </p>
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
-  );
+interface OptionProp {
+  value: string;
+  label: string;
+  icon?: React.ReactNode;
 }
+
+interface SelectProps {
+  leadingIcon?: React.ReactNode;
+  options?: OptionProp[];
+  selectedValue?: OptionProp;
+  onChange?: (value: string) => void;
+  placeholder?: string;
+}
+
+const AppSelect = React.forwardRef<HTMLDivElement, SelectProps>(
+  ({ options, selectedValue, onChange, leadingIcon, placeholder = 'Select' }) => {
+    return (
+      <Select onValueChange={onChange} defaultValue={selectedValue?.value}>
+        <SelectTrigger className="w-full border border-gray-300 bg-white focus:border-primary-900 focus:ring-0">
+          <SelectValue
+            placeholder={
+              <p className="flex w-full items-center gap-2">
+                {leadingIcon}
+                <span>{placeholder}</span>
+              </p>
+            }
+          ></SelectValue>
+        </SelectTrigger>
+        <SelectContent className="bg-white">
+          {options?.map((option, index) => (
+            <SelectItem value={option?.value} key={index}>
+              <p className="flex items-center gap-2">
+                {option?.icon}
+                {option?.label}
+              </p>
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    );
+  }
+);
+AppSelect.displayName = 'AppSelect';
+export default AppSelect;
