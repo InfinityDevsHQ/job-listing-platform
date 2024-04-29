a8d58451fc36e93d1c41d00737640ba5a4b3fceb
bytegenious01 <bytegenius@infinitydevs.io>
2024-04-29 09:45:06 -0700

    feat: unsubscribe to news letter api

--- src/lib/newsletter.ts
+++ src/lib/newsletter.ts
@@ -7,6 +7,7 @@ const NEWSLETTER_URLS = {
   allSubscribers: `${PLATFORM_API_BASE_URL}/api/v1/newsletter/subscribers`,
   subscribersCount: `${PLATFORM_API_BASE_URL}/api/v1/newsletter/subscribers/count`,
   subscribe: `${PLATFORM_API_BASE_URL}/api/v1/newsletter/subscribe`,
+  unsubscribe: `${PLATFORM_API_BASE_URL}/api/v1/newsletter/unsubscribe/`,
 };
 
 export async function getSubscribers(): Promise<Subscriber[]> {
@@ -19,3 +20,7 @@ export async function getSubscribersCount(): Promise<number> {
 }
 export const subscribe = (body: SubscribeBody): Promise<SubscribeBody> =>
   DataService.post<SubscribeBody>(NEWSLETTER_URLS.subscribe, body);
+export const unsubscribe = async (email: string) => {
+  const data = await DataService.delete(`${NEWSLETTER_URLS.unsubscribe}/${email}`);
+  return data;
+};
