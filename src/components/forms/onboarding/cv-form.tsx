0b144a80ad643a61c2128fceae3700589a7a3e9c
bytegenious01 <bytegenius@infinitydevs.io>
2024-04-30 04:07:24 -0700

    fix: cv form replaced with shadcn and validation

--- src/components/forms/onboarding/cv-form.tsx
+++ src/components/forms/onboarding/cv-form.tsx
@@ -1,20 +1,45 @@
 'use client';
+import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
+import Input from '@/components/ui/input';
 import Pagination from '@/components/ui/pagination';
-import UploadImage from '@/components/ui/upload-image';
+import { useQueryParams } from '@/hooks/useQueryParams';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
-const FileSchema = z.object({
-  name: z.string(),
-  size: z.number(),
-  type: z.string(),
-  preview: z.string(),
+const fileSchema = z.object({
+  fileName: z.string().min(2, { message: 'Please Upload a valid file' }),
 });
 export default function CVForm() {
-  const form = useForm({});
+  const addQueryParams = useQueryParams();
+  const form = useForm<z.infer<typeof fileSchema>>({
+    resolver: zodResolver(fileSchema),
+    defaultValues: {
+      fileName: '',
+    },
+  });
+  async function onSubmit(values: z.infer<typeof fileSchema>) {
+    console.log(values);
+    addQueryParams('step', 'filter-jobs');
+  }
   return (
-    <form className="flex h-full flex-col gap-4 py-8 lg:gap-8">
-      <UploadImage />
-      <Pagination />
-    </form>
+    <Form {...form}>
+      <form
+        className="flex h-full flex-col gap-4 py-8 lg:gap-8"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <FormField
+          name="fileName"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl {...field}>
+                <Input type="File" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Pagination isNextSubmit />
+      </form>
+    </Form>
   );
 }
