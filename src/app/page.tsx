a4d72e2d5d4eab51166500b4da3e89d010fc7e8a
bytegenious01 <bytegenius@infinitydevs.io>
2024-04-30 01:41:17 -0700

    feat: loadMore button functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
 import CompaniesList from '@/components/companies/companies-list';
 import JobsList from '@/components/jobs/jobs-list';
 import FireIcon from '@/components/svgs/fire';
+import Button from '@/components/ui/button';
 import Hero from '@/components/ui/hero';
 import SectionHeader from '@/components/ui/section-header';
 import { getPromotedCompanies } from '@/lib/companies';
 import { getJobs } from '@/lib/jobs';
 
 export default async function Home() {
-  const jobs = await getJobs();
+  const jobs = await getJobs({});
   const companies = await getPromotedCompanies();
+  let skip = 10;
+  let limit = 25;
+  console.log('Jobs', jobs.length);
+  const loadMore = async () => {
+    skip = skip + 25;
+    limit = limit + 25;
+    const newJobs = await getJobs({ skip, limit });
+    jobs.push(...newJobs);
+  };
   return (
     <div className="grid grid-cols-3 gap-8 p-4 lg:p-16">
       <div className="col-span-3 flex flex-col gap-4 lg:col-span-2 lg:gap-8">
@@ -19,6 +29,7 @@ export default async function Home() {
           helpText={`${jobs?.length} JOBS`}
         />
         <JobsList jobs={jobs} />
+        <Button text="Load More" variant="primary" className="self-end" />
       </div>
       <div className="col-span-3 flex flex-col gap-5 lg:col-span-1">
         <SectionHeader
--- src/components/jobs/jobs-list.tsx
+++ src/components/jobs/jobs-list.tsx
@@ -1,12 +1,10 @@
 import { JobListProps } from '@/types/types';
-import Button from '../ui/button';
 import JobCard from './_components/job-card';
 
-const JobsList = async ({ jobs }: JobListProps) => {
+const JobsList = ({ jobs }: JobListProps) => {
   return (
     <div className="flex flex-col gap-4 lg:gap-8">
       {jobs?.map((job, index) => <JobCard key={index} job={job} />)}
-      <Button text="Load More" variant="primary" className="self-end" />
     </div>
   );
 };
