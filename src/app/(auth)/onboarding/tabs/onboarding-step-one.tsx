21212ee6302e2504848120844f7239b61b244626
bytegenious01 <bytegenius@infinitydevs.io>
2024-05-01 09:55:41 -0700

    refactor: used promise all settled for increasing performance

--- src/app/(auth)/onboarding/tabs/onboarding-step-one.tsx
+++ src/app/(auth)/onboarding/tabs/onboarding-step-one.tsx
@@ -5,21 +5,31 @@ import Image from 'next/image';
 import BoardingHeader from '../_components/boarding-header';
 
 export default async function onboardingStepOne() {
-  const [languages, countries] = await Promise.all([getLanguages(), getCountries()]);
-  return (
-    <div className="grid w-full grid-cols-2">
-      <div className="col-span-2 hidden items-center justify-center lg:col-span-1 lg:flex">
-        <Image
-          src={'/assets/boarding/location_poster.png'}
-          alt="Location Poster"
-          width={580}
-          height={580}
-        />
+  const promises = [getLanguages(), getCountries()];
+
+  try {
+    const results = await Promise.allSettled(promises);
+    const languages = results[0].status === 'fulfilled' ? results[0].value : [];
+    const countries = results[1].status === 'fulfilled' ? results[1].value : [];
+    console.log(languages);
+    return (
+      <div className="grid w-full grid-cols-2">
+        <div className="col-span-2 hidden items-center justify-center lg:col-span-1 lg:flex">
+          <Image
+            src={'/assets/boarding/location_poster.png'}
+            alt="Location Poster"
+            width={580}
+            height={580}
+          />
+        </div>
+        <div className="col-span-2 flex w-full flex-col justify-center gap-4 lg:col-span-1">
+          <BoardingHeader title="1. Location" />
+          <LocationForm languages={languages} countries={countries} />
+        </div>
       </div>
-      <div className="col-span-2 flex w-full flex-col justify-center gap-4 lg:col-span-1">
-        <BoardingHeader title="1. Location" />
-        <LocationForm languages={languages} countries={countries} />
-      </div>
-    </div>
-  );
+    );
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw new Error(`Something Went Wrong  ${error}`);
+  }
 }
