9d16f7b83ffa0e6cc0d8d7600244e472b2c8af97
bytegenious01 <bytegenius@infinitydevs.io>
2024-05-03 06:33:42 -0700

    feat: apply job success additional info form

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,9 +1,10 @@
+import ApplyJobAdditionInfoForm from '@/components/forms/apply-job-additional-info-form/apply-job-additional-info-form';
 import Image from 'next/image';
 const ApplyJobSuccess = () => {
   return (
-    <section className="gap-4 rounded-md border border-gray-200 bg-gray-50 p-4 lg:gap-8 lg:p-8">
+    <section className="flex flex-col gap-4 rounded-md border border-gray-200 bg-gray-50 p-4 lg:gap-8 lg:p-8">
       <div className="relative mt-4 flex items-center justify-center bg-white lg:mt-8">
-        <Image src="/assets/party.png" width={1684} height={1820} alt="party" />
+        <Image src="/assets/party.png" width={1000} height={1000} alt="party" />
         <div className="absolute flex flex-col gap-4 lg:gap-8">
           <h3 className="text-center font-sans text-lg font-bold text-gray-700 lg:text-6xl">
             Congratulations!
@@ -18,6 +19,7 @@ const ApplyJobSuccess = () => {
           <h4 className="text-center text-base font-bold text-gray-500 lg:text-3xl">Google</h4>
         </div>
       </div>
+      <ApplyJobAdditionInfoForm />
     </section>
   );
 };
---
+++ src/components/forms/apply-job-additional-info-form/apply-job-additional-info-form.tsx
@@ -0,0 +1,125 @@
+'use client';
+import AppSelect from '@/components/ui/app-select';
+import Button from '@/components/ui/button';
+import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
+import Input from '@/components/ui/input';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Send } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+const MinimumSalary = ['$ 100', '$ 200', '$ 800'];
+const Amount = ['$ 100', '$ 200', '$ 800'];
+const Hours = ['2', '4', '8'];
+const additionInfoFormSchema = z.object({
+  email: z.string().email({ message: 'Please Enter a valid email address' }),
+  minimum_salary: z.string().min(1, { message: 'Please Select One' }),
+  amount: z.string().min(1, { message: 'Please Select One' }),
+  hours: z.string().min(1, { message: 'Please Select One' }),
+});
+const ApplyJobAdditionInfoForm = () => {
+  const form = useForm({
+    resolver: zodResolver(additionInfoFormSchema),
+    defaultValues: {
+      email: '',
+      minimum_salary: '',
+      amount: '',
+      hours: '',
+    },
+  });
+  async function onSubmit(values: z.infer<typeof additionInfoFormSchema>) {
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form
+        className="grid grid-cols-4 gap-4 border border-gray-200 p-4 pt-8"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <h5 className="col-span-4 text-base font-bold text-gray-700">
+          Increase your chances of success!
+        </h5>
+        <p className="col-span-4 text-sm">
+          Even in companyName, the following additional information about you is welcome. Filling it
+          out is not mandatory, but it may help you get this job.
+        </p>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem className="col-span-4">
+              <FormControl>
+                <Input placeholder="Email" type="email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="minimum_salary"
+          render={({ field }) => (
+            <FormItem className="col-span-2">
+              <FormControl>
+                <AppSelect
+                  {...field}
+                  placeholder="Minimum Salary"
+                  options={MinimumSalary.map((option) => ({
+                    label: option,
+                    value: option,
+                  }))}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="amount"
+          render={({ field }) => (
+            <FormItem className="col-span-2 lg:col-span-1">
+              <FormControl>
+                <AppSelect
+                  placeholder="$"
+                  {...field}
+                  options={Amount.map((option) => ({
+                    label: option,
+                    value: option,
+                  }))}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="hours"
+          render={({ field }) => (
+            <FormItem className="col-span-4 lg:col-span-1">
+              <FormControl>
+                <AppSelect
+                  placeholder="Hours"
+                  {...field}
+                  options={Hours.map((option) => ({
+                    label: option,
+                    value: option,
+                  }))}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          text="Send"
+          type="submit"
+          trailingIcon={<Send className="rotate-45" size={16} />}
+          variant="primary"
+          className="col-span-4 ml-auto place-items-end"
+        />
+      </form>
+    </Form>
+  );
+};
+export default ApplyJobAdditionInfoForm;
