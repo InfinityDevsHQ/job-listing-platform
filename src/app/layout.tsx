bc947e9215ac316871b5a49657da31c158d71f5d
bytegenious01 <bytegenius@infinitydevs.io>
2024-05-03 03:46:40 -0700

    feat: initial setup for react query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
-import { GeistSans } from 'geist/font/sans';
-import type { Metadata } from 'next';
-import './globals.css';
-
 import Footer from '@/components/footer/page';
 import Header from '@/components/header/top-bar';
 import { Toaster } from '@/components/ui/sonner';
-
+import { GeistSans } from 'geist/font/sans';
+import type { Metadata } from 'next';
+import './globals.css';
+import Providers from './providers';
 // Get meta data from relevant persons
 export const metadata: Metadata = {
   title: 'Click Job AI',
@@ -21,7 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${GeistSans.variable} mx-auto h-auto max-w-screen-2xl`}>
         <Header />
-        <main className="h-full w-full">{children}</main>
+        <main className="h-full w-full">
+          <Providers>{children}</Providers>
+        </main>
         <Footer />
         <Toaster richColors />
       </body>
