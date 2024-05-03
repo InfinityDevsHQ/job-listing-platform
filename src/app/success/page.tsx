ff3a3fac0fc2b863c3f4e249e9b68fdebffc6417
bytegenious01 <bytegenius@infinitydevs.io>
2024-05-03 03:15:53 -0700

    feat: apply job success page

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,33 +1,20 @@
 import Image from 'next/image';
-
-export default function JobPostSuccess() {
+const ApplyJobSuccess = () => {
   return (
     <section className="relative mt-4 flex items-center justify-center bg-white lg:mt-8">
       <Image src="/assets/party.png" width={1684} height={1820} alt="party" />
       <div className="absolute flex flex-col gap-4 lg:gap-8">
-        <div className="flex items-center justify-center">
-          <Image
-            src="/assets/success.png"
-            alt="Flowers"
-            width={400}
-            height={305}
-            className="hidden lg:inline-block"
-          />
-          <Image
-            src="/assets/success.png"
-            alt="Flowers"
-            width={200}
-            height={153}
-            className="lg:hidden"
-          />
-        </div>
         <h3 className="text-center font-sans text-xl font-bold text-gray-700 lg:text-6xl">
           Congratulations!
         </h3>
         <p className="text-center font-sans text-sm text-gray-500 lg:text-3xl">
-          You Have Successfully Posted Job
+          You Have Successfully Applied for position
         </p>
+        <h4 className="text-center font-bold text-gray-500 lg:text-5xl">Python Developer</h4>
+        <p className="text-center font-sans text-sm text-gray-500 lg:text-3xl">To Company</p>
+        <h4 className="text-center font-bold text-gray-500 lg:text-3xl">Google</h4>
       </div>
     </section>
   );
-}
+};
+export default ApplyJobSuccess;
