e02432458daec2ec02a4a20dee7b100902779301
Saad Gulzar <isaadgulzar@gmail.com>
2024-05-07 00:27:31 +0500

    refactor: update scrollbar and recruit page ui bug

--- src/app/recruit/page.tsx
+++ src/app/recruit/page.tsx
@@ -301,7 +301,7 @@ const Recruit = () => {
             asked some of them what they appreciate about ClickJob.
           </p>
           <div className="grid gap-4 lg:grid-cols-4">
-            <div className="flex flex-col gap-4 rounded-md border border-gray-300 bg-white p-4 shadow-theme md:basis-1/2 lg:basis-1/6 lg:p-8">
+            <div className="flex flex-col gap-4 rounded-md border border-neutral-200 bg-white p-4 shadow-theme md:basis-1/2 lg:basis-1/6 lg:p-8">
               <Image
                 src="/assets/companies/logos/company- (1).png"
                 width={295}
@@ -318,7 +318,7 @@ const Recruit = () => {
                 user_role="Software Engineer"
               />
             </div>
-            <div className="flex flex-col gap-4 rounded-md border border-gray-300 bg-white p-4 shadow-theme md:basis-1/2 lg:basis-1/6 lg:p-8">
+            <div className="flex flex-col gap-4 rounded-md border border-neutral-200 bg-white p-4 shadow-theme md:basis-1/2 lg:basis-1/6 lg:p-8">
               <Image
                 src="/assets/companies/logos/company- (2).png"
                 width={295}
@@ -335,7 +335,7 @@ const Recruit = () => {
                 user_role="Software Engineer"
               />
             </div>
-            <div className="flex flex-col gap-4 rounded-md border border-gray-300 bg-white p-4 shadow-theme md:basis-1/2 lg:basis-1/6 lg:p-8">
+            <div className="flex flex-col gap-4 rounded-md border border-neutral-200 bg-white p-4 shadow-theme md:basis-1/2 lg:basis-1/6 lg:p-8">
               <Image
                 src="/assets/companies/logos/company- (3).png"
                 width={295}
@@ -352,7 +352,7 @@ const Recruit = () => {
                 user_role="Software Engineer"
               />
             </div>
-            <div className="flex flex-col gap-4 rounded-md border border-gray-300 bg-white p-4 shadow-theme md:basis-1/2 lg:basis-1/6 lg:p-8">
+            <div className="flex flex-col gap-4 rounded-md border border-neutral-200 bg-white p-4 shadow-theme md:basis-1/2 lg:basis-1/6 lg:p-8">
               <Image
                 src="/assets/companies/logos/company- (4).png"
                 width={295}
@@ -369,7 +369,7 @@ const Recruit = () => {
                 user_role="Software Engineer"
               />
             </div>
-            <div className="flex flex-col gap-4 rounded-md border border-gray-300 bg-white p-4 shadow-theme md:basis-1/2 lg:basis-1/6 lg:p-8">
+            <div className="flex flex-col gap-4 rounded-md border border-neutral-200 bg-white p-4 shadow-theme md:basis-1/2 lg:basis-1/6 lg:p-8">
               <Image
                 src="/assets/companies/logos/company- (5).png"
                 width={295}
@@ -386,7 +386,7 @@ const Recruit = () => {
                 user_role="Software Engineer"
               />
             </div>
-            <div className="flex flex-col gap-4 rounded-md border border-gray-300 bg-white p-4 shadow-theme md:basis-1/2 lg:basis-1/6 lg:p-8">
+            <div className="flex flex-col gap-4 rounded-md border border-neutral-200 bg-white p-4 shadow-theme md:basis-1/2 lg:basis-1/6 lg:p-8">
               <Image
                 src="/assets/companies/logos/company- (6).png"
                 width={295}
@@ -403,7 +403,7 @@ const Recruit = () => {
                 user_role="Software Engineer"
               />
             </div>
-            <div className="flex flex-col gap-4 rounded-md border border-gray-300 bg-white p-4 shadow-theme md:basis-1/2 lg:basis-1/6 lg:p-8">
+            <div className="flex flex-col gap-4 rounded-md border border-neutral-200 bg-white p-4 shadow-theme md:basis-1/2 lg:basis-1/6 lg:p-8">
               <Image
                 src="/assets/companies/logos/company- (7).png"
                 width={295}
@@ -420,7 +420,7 @@ const Recruit = () => {
                 user_role="Software Engineer"
               />
             </div>
-            <div className="flex flex-col gap-4 rounded-md border border-gray-300 bg-white p-4 shadow-theme md:basis-1/2 lg:basis-1/6 lg:p-8">
+            <div className="flex flex-col gap-4 rounded-md border border-neutral-200 bg-white p-4 shadow-theme md:basis-1/2 lg:basis-1/6 lg:p-8">
               <Image
                 src="/assets/companies/logos/company- (8).png"
                 width={295}
