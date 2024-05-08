d97c25ab0a47e89bfb6b26ea2763e2ce2798399b
bytegenious01 <bytegenius@infinitydevs.io>
2024-05-08 01:18:08 -0700

    feat: post jobs z-pattern section

--- src/app/jobs/create/_components/advanced-promotion-card.tsx
+++ src/app/jobs/create/_components/advanced-promotion-card.tsx
@@ -3,11 +3,13 @@ type AdvancedPromotionCardProps = {
   imagUrl: string;
   title: string;
   points: string[];
+  reverse?: boolean;
 };
 export default function AdvancedPromotionCard({
   imagUrl,
   title,
   points,
+  reverse,
 }: AdvancedPromotionCardProps) {
   return (
     <div
@@ -16,16 +18,21 @@ export default function AdvancedPromotionCard({
         background: 'linear-gradient(92.04deg, #D8E8FF 1.76%, #F5FAFF 102.29%)',
       }}
     >
-      <div className="flex flex-col justify-center gap-4  lg:flex-row lg:items-center lg:gap-8">
-        <div>
-          <Image src={imagUrl} alt={title} width={200} height={200.92} className="lg:hidden" />
-          <Image src={imagUrl} alt={title} width={539} height={539} className="hidden lg:block" />
+      <div className="flex flex-col justify-center gap-4 lg:flex-row lg:items-center lg:gap-8">
+        <div
+          className={`flex items-center justify-center ${reverse ? 'lg:order-2' : 'lg:order-1'}`}
+        >
+          <Image src={imagUrl} alt={title} width={539} height={539} className="h-auto w-auto" />
         </div>
-        <div className="flex flex-col gap-4 lg:gap-12">
-          <h3 className="font-sans text-base font-bold text-gray-700 lg:mb-0.5">{title}</h3>
-          <ol className="flex flex-col gap-4">
+        <div
+          className={`flex flex-1 flex-col gap-4 lg:gap-12 ${reverse ? 'lg:order-1' : 'lg:order-2'}`}
+        >
+          <h3 className="text-center text-base font-bold text-gray-700 lg:mb-0.5 lg:text-left lg:text-4xl">
+            {title}
+          </h3>
+          <ol className="flex list-disc flex-col gap-4 pl-4">
             {points.map((point, index) => (
-              <li key={index} className="font-sans text-sm text-gray-700">
+              <li key={index} className="font-sans text-sm text-gray-700 lg:text-2xl">
                 {point}
               </li>
             ))}
--- src/app/jobs/create/_components/advanced-promotion.tsx
+++ src/app/jobs/create/_components/advanced-promotion.tsx
@@ -16,10 +16,27 @@ export default function AdvancedPromotion() {
       />
       <AdvancedPromotionCard
         title="Hot jobs is the foundation"
-        imagUrl="/assets/post-jobs/post-1.png"
+        imagUrl="/assets/post-jobs/post-2.png"
+        reverse
         points={[
           'Offers with Hot Jobs average 2-3 times more views compared to offers with the basic package',
           'Offers with HotJobs have access to a talent database, where you can actively reach out to relevent users who are open to new job opportunities. The price for HotJobs is 2,749 CZK for 14 days, or 4,999 CZK for 30 days, per offer.',
+        ]}
+      />
+      <AdvancedPromotionCard
+        title="With TOP3, you'll always be on top."
+        imagUrl="/assets/post-jobs/post-3.png"
+        points={[
+          `Thanks to your offer being on one of the first three positions on every page, it won't escape the attention of any applicant.`,
+        ]}
+      />
+      <AdvancedPromotionCard
+        title="Be visible. Build your brand with SJ Ads"
+        imagUrl="/assets/post-jobs/post-4.png"
+        reverse
+        points={[
+          'Our banners, native advertising, and PR articles will get you in front of eyes of hundreds of thousand of our visitors',
+          'You can use them for promoting ads, employer branding, and gaining new business contacts.',
         ]}
       />
     </section>
