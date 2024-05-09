51edbf03297a8a8b465e55688c0a17753613ea44
bytegenious01 <bytegenius@infinitydevs.io>
2024-05-09 07:19:23 -0700

    feat: separate hook for companies data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,28 +9,30 @@ export default function Home() {
   const { isLoading, error, data: jobs } = useAllJobs({ isHot: true });
   if (isLoading) return <p>Loading......</p>;
   if (error) return <p>Error while getting data: {error.message}</p>;
-  return (
-    <div className="grid grid-cols-3 gap-8 p-4 lg:p-8">
-      <div className="col-span-3 flex flex-col gap-4 lg:col-span-2 lg:gap-8">
-        <Hero
-          heading="Find your dream job here"
-          desc="Explore the latest job openings and apply for the best job opportunities available today!"
-          vectorUrl="/assets/images/home/homePage_hero_section_vector.png"
-        />
-        <SectionHeader
-          leadingIcon={<FireIcon className="h-5 w-4 text-red-900 lg:h-7 lg:w-6" />}
-          heading="Latest Hot Offers"
-          helpText={`${jobs?.length} JOBS`}
-        />
-        <JobsList jobs={jobs} />
+  if (jobs) {
+    return (
+      <div className="grid grid-cols-3 gap-8 p-4 lg:p-8">
+        <div className="col-span-3 flex flex-col gap-4 lg:col-span-2 lg:gap-8">
+          <Hero
+            heading="Find your dream job here"
+            desc="Explore the latest job openings and apply for the best job opportunities available today!"
+            vectorUrl="/assets/images/home/homePage_hero_section_vector.png"
+          />
+          <SectionHeader
+            leadingIcon={<FireIcon className="h-5 w-4 text-red-900 lg:h-7 lg:w-6" />}
+            heading="Latest Hot Offers"
+            helpText={`${jobs?.length} JOBS`}
+          />
+          <JobsList jobs={jobs} />
+        </div>
+        <div className="col-span-3 flex flex-col gap-5 lg:col-span-1">
+          <SectionHeader
+            leadingIcon={<FireIcon className="h-5 w-4 text-black lg:h-7 lg:w-6" />}
+            heading="Companies That Will Grow You Forward"
+          />
+          <div className="overflow-x-auto">{/* <CompaniesList companies={companies} /> */}</div>
+        </div>
       </div>
-      <div className="col-span-3 flex flex-col gap-5 lg:col-span-1">
-        <SectionHeader
-          leadingIcon={<FireIcon className="h-5 w-4 text-black lg:h-7 lg:w-6" />}
-          heading="Companies That Will Grow You Forward"
-        />
-        <div className="overflow-x-auto">{/* <CompaniesList companies={companies} /> */}</div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
--- src/hooks/usePromotedCompanies.ts
+++ src/hooks/usePromotedCompanies.ts
@@ -2,7 +2,7 @@ import { getPromotedCompanies } from '@/lib/companies';
 import { useQuery } from '@tanstack/react-query';
 export function usePromotedCompanies() {
   return useQuery({
-    queryKey: ['allJobs'],
-    queryFn: async () => getPromotedCompanies(),
+    queryKey: ['promotedCompanies'],
+    queryFn: async () => await getPromotedCompanies(),
   });
 }
