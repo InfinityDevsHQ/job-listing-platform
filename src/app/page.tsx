579a5c77c1bd458a128ecef573248ba9fbd53be4
bytegenious01 <bytegenius@infinitydevs.io>
2024-05-09 02:59:41 -0700

    feat: show cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import CompaniesList from '@/components/companies/companies-list';
+import JobsList from '@/components/jobs/jobs-list';
+import FireIcon from '@/components/svgs/fire';
+import { Button } from '@/components/ui/button-new';
+import Hero from '@/components/ui/hero';
+import SectionHeader from '@/components/ui/section-header';
 import { getPromotedCompanies } from '@/lib/companies';
 import { getJobs } from '@/lib/jobs';
 
@@ -14,11 +20,30 @@ export default async function Home() {
     jobs.push(...newJobs);
   };
   return (
-    <div>
-      <h2 className="text-4xl font-bold">Jobs data</h2>
-      <pre className="max-w-10">{JSON.stringify(jobs, null, 2)}</pre>
-      <h2 className="text-4xl font-bold">Companies data</h2>
-      <pre className="max-w-10">{JSON.stringify(companies, null, 2)}</pre>
+    <div className="grid grid-cols-3 gap-8 p-4 lg:p-8">
+      <div className="col-span-3 flex flex-col gap-4 lg:col-span-2 lg:gap-8">
+        <Hero
+          heading="Find your dream job here"
+          desc="Explore the latest job openings and apply for the best job opportunities available today!"
+          vectorUrl="/assets/images/home/homePage_hero_section_vector.png"
+        />
+        <SectionHeader
+          leadingIcon={<FireIcon className="h-5 w-4 text-red-900 lg:h-7 lg:w-6" />}
+          heading="Latest Hot Offers"
+          helpText={`${jobs?.length} JOBS`}
+        />
+        <JobsList jobs={jobs} />
+        <Button className="self-end">Load More</Button>
+      </div>
+      <div className="col-span-3 flex flex-col gap-5 lg:col-span-1">
+        <SectionHeader
+          leadingIcon={<FireIcon className="h-5 w-4 text-black lg:h-7 lg:w-6" />}
+          heading="Companies That Will Grow You Forward"
+        />
+        <div className="overflow-x-auto">
+          <CompaniesList companies={companies} />
+        </div>
+      </div>
     </div>
   );
 }
