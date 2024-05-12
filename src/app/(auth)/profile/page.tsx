7cd85b51d12590e1e5ce9ceaefe1f50cc247fd64
bytegenious01 <bytegenius@infinitydevs.io>
2024-05-12 05:09:27 -0700

    feat: wip user profile apis

--- src/app/(auth)/profile/page.tsx
+++ src/app/(auth)/profile/page.tsx
@@ -1,29 +1,43 @@
-'use client';
-import ProfileTabs from '@/components/profile/profile-tabs';
-import UserInfoMobilePanel from '@/components/profile/user-info-mobile-panel';
-import UserInfoPanel from '@/components/profile/user-info-panel';
-import useAuthStore from '@/stores/authStore/store';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { getUserProfile, updateUser } from '@/lib/user';
 
-const Profile = () => {
-  const [editProfile, setEditProfile] = useState<boolean>(false);
-  const [profileVal, setProfileVal] = useState(
-    `I am looking to enhance my skills in advanced web development and contribute to innovative projects. Seeking opportunities to excel in UI design and collaborative environments. Passionate about creating efficient and user-friendly web systems.`
-  );
-
-  const router = useRouter();
-  const user = useAuthStore((state) => state.user);
-
+const Profile = async () => {
+  const user = await getUserProfile();
+  const updateUserBody = {
+    email: 'new@gmail.com',
+    name: 'John Doe',
+    language: 'Persion',
+    latitude: 100,
+    longitude: 200,
+    city: 'Manchester',
+    country: 'United Kingdom',
+    timezone: 'UK',
+    phone_number: '0000099393',
+    profile_picture: '/assets/avatar',
+    prefered_language: 'English',
+    online_status: 'Online',
+    selected_country: 'United Kingdom',
+    geo_resolve_tries: 'Pro',
+    is_onboarded: true,
+  };
+  const updatedUser = await updateUser(updateUserBody);
+  // TODO: put all all useStates and other hooks into child nodes,
+  // const [editProfile, setEditProfile] = useState<boolean>(false);
+  // const [profileVal, setProfileVal] = useState(
+  //   `I am looking to enhance my skills in advanced web development and contribute to innovative projects. Seeking opportunities to excel in UI design and collaborative environments. Passionate about creating efficient and user-friendly web systems.`
+  // );
+  // const router = useRouter();
+  // const user = useAuthStore((state) => state.user);
   return (
     <div className="grid gap-4 p-4 lg:grid-cols-4 lg:gap-8 lg:p-8">
-      <UserInfoPanel online_status={user.online_status} />
-      <UserInfoMobilePanel online_status={user.online_status} />
-      <ProfileTabs
+      <pre>{JSON.stringify(user)}</pre>
+      <pre>{JSON.stringify(updatedUser)}</pre>
+      {/* <UserInfoPanel online_status={user.online_status} /> */}
+      {/* <UserInfoMobilePanel online_status={user.online_status} /> */}
+      {/* <ProfileTabs
         editProfile={editProfile}
         setEditProfile={setEditProfile}
         profileVal={profileVal}
-      />
+      /> */}
     </div>
   );
 };
