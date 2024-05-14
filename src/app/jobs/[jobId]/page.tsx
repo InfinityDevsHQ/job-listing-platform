2d0ecd47662d0c0c9f67480b475826ed1df6e758
bytegenious01 <bytegenius@infinitydevs.io>
2024-05-14 06:59:23 -0700

    feat: jobs/id using react query

--- src/app/jobs/[jobId]/page.tsx
+++ src/app/jobs/[jobId]/page.tsx
@@ -1,30 +1,31 @@
+import { getQueryClient } from '@/app/utils/rq/react-query-client';
+import { ReactQueryHydrate } from '@/app/utils/rq/react-query-hydrate';
 import JobsList from '@/components/jobs/jobs-list';
 import FireIcon from '@/components/svgs/fire';
 import SectionHeader from '@/components/ui/section-header';
-import { getJobById, getSimilarJobs } from '@/lib/jobs';
+import { GET_SINGLE_JOB_LISTING_BY_ID_KEY } from '@/hooks/useJobListingById';
+import { getJobById } from '@/lib/jobs';
+import { dehydrate } from '@tanstack/react-query';
 import { notFound } from 'next/navigation';
 import JobDetails from '../_components/job-details';
-
 type JobDetailsSlug = {
   params: { jobId: string };
 };
-
 const JobDetailsPage = async ({ params }: JobDetailsSlug) => {
   const jobListingId = params.jobId;
-
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: [GET_SINGLE_JOB_LISTING_BY_ID_KEY],
+    queryFn: () => getJobById(jobListingId),
+  });
   if (!jobListingId) {
     return notFound();
   }
-
-  const job = await getJobById(jobListingId);
-  const similarJobsData = await getSimilarJobs(jobListingId);
-  if (!job.id) {
-    return notFound();
-  }
-
   return (
     <div className="flex flex-col gap-4 p-4 lg:gap-8 lg:p-8">
-      <JobDetails job={job} />
+      <ReactQueryHydrate state={dehydrate(queryClient)}>
+        <JobDetails jobId={jobListingId} />
+      </ReactQueryHydrate>
       <div className="flex w-full flex-col gap-4 lg:w-9/12 lg:gap-8">
         <SectionHeader heading="Similar Job Offers" leadingIcon={<FireIcon />} />
         <JobsList similarJobId={jobListingId} />
--- src/hooks/useJobListingById.ts
+++ src/hooks/useJobListingById.ts
@@ -1,8 +1,9 @@
 import { getJobById } from '@/lib/jobs';
 import { useQuery } from '@tanstack/react-query';
-export function useJobListingById({ jobId }: { jobId: string }) {
+export const GET_SINGLE_JOB_LISTING_BY_ID_KEY = 'promotedCompanies';
+export function useJobListingById(jobId: string) {
   return useQuery({
-    queryKey: ['singleJobListing'],
+    queryKey: [GET_SINGLE_JOB_LISTING_BY_ID_KEY],
     queryFn: () => getJobById(jobId),
   });
 }
