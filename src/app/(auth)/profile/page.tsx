800216415c98c072701ca4bcd9d0805557ecf7f2
bytegenious01 <bytegenius@infinitydevs.io>
2024-05-14 03:57:37 -0700

    refactor: cleaned up profile page component

--- src/app/(auth)/profile/page.tsx
+++ src/app/(auth)/profile/page.tsx
@@ -1,17 +1,8 @@
 import UserInfoPanel from '@/components/profile/user-info-panel';
 
 const Profile = async () => {
-  // TODO: put all all useStates and other hooks into child nodes,
-  // const [editProfile, setEditProfile] = useState<boolean>(false);
-  // const [profileVal, setProfileVal] = useState(
-  //   `I am looking to enhance my skills in advanced web development and contribute to innovative projects. Seeking opportunities to excel in UI design and collaborative environments. Passionate about creating efficient and user-friendly web systems.`
-  // );
-  // const router = useRouter();
-  // const user = useAuthStore((state) => state.user);
   return (
     <div className="grid gap-4 p-4 lg:grid-cols-4 lg:gap-8 lg:p-8">
-      {/* <pre>{JSON.stringify(user)}</pre> */}
-      {/* <pre>{JSON.stringify(updatedUser)}</pre> */}
       <UserInfoPanel />
     </div>
   );
