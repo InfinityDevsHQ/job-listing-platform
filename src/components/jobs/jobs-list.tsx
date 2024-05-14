ce6e29b3f8575282b2f8be6bf26ae11b4648f547
bytegenious01 <bytegenius@infinitydevs.io>
2024-05-14 01:25:44 -0700

    feat: infinite query for all jobs

--- src/components/jobs/jobs-list.tsx
+++ src/components/jobs/jobs-list.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useGetHotJobs } from '@/app/utils/rq/hooks/jobs-hook';
+import { useAllJobs } from '@/hooks/useAllJobs';
 import { cn } from '@/lib/utils';
 import { JobListProps } from '@/types/types';
 import { RefreshCcwIcon } from 'lucide-react';
@@ -7,18 +8,30 @@ import { Button } from '../ui/button-new';
 import JobCard from './_components/job-card';
 
 const JobsList = ({ hotJobsAll, similarJobId, allJobs }: JobListProps) => {
-  const { fetchNextPage, data, hasNextPage, isFetchingNextPage } = useGetHotJobs();
-  const hotJobs = data?.pages.flat() || [];
+  const {
+    fetchNextPage: fetchNextHotJobs,
+    data: hotJobsFiltered,
+    hasNextPage: hasNextHotJobsPage,
+    isFetchingNextPage: isFetchingNextHotJobsPage,
+  } = useGetHotJobs();
+  const hotJobs = hotJobsFiltered?.pages.flat() || [];
+
+  const {
+    fetchNextPage: fetchNextJobs,
+    data: allJobsData,
+    hasNextPage: hasNextAllJobsPage,
+    isFetchingNextPage: isFetchingNextAllJobsPage,
+  } = useAllJobs();
   return (
     <div className="flex flex-col gap-4 lg:gap-8">
       {hotJobsAll && hotJobs?.map((job, index) => <JobCard key={index} job={job} />)}
       {/* {similarJobId && similarJobsData?.map((job, index) => <JobCard key={index} job={job} />)} */}
-      {hasNextPage && (
+      {hasNextHotJobsPage && (
         <div className="flex items-center justify-center">
-          <Button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
+          <Button onClick={() => fetchNextHotJobs()} disabled={isFetchingNextHotJobsPage}>
             LoadMore
             <RefreshCcwIcon
-              className={cn('ml-2 h-4 w-4', { 'animate-spin': isFetchingNextPage })}
+              className={cn('ml-2 h-4 w-4', { 'animate-spin': isFetchingNextHotJobsPage })}
             />
           </Button>
         </div>
--- src/hooks/useAllJobs.ts
+++ src/hooks/useAllJobs.ts
@@ -1,9 +1,14 @@
 import { getJobs } from '@/lib/jobs';
-import { useQuery } from '@tanstack/react-query';
-
-export function useAllJobs({ isHot }: { isHot: boolean }) {
-  return useQuery({
-    queryKey: ['allJobs'],
-    queryFn: () => getJobs({ is_hot: isHot }),
+import { useInfiniteQuery } from '@tanstack/react-query';
+export const ALL_JOBS_KEY = 'allJobs';
+export function useAllJobs() {
+  return useInfiniteQuery({
+    initialPageParam: 0,
+    retry: 1,
+    queryKey: [ALL_JOBS_KEY],
+    queryFn: ({ pageParam }) => getJobs({ is_hot: false, skip: pageParam, limit: 10 }),
+    getNextPageParam: ({ lastPageParam }) => {
+      return lastPageParam.length + 1;
+    },
   });
 }
