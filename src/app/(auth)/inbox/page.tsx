6ab23475bd0afcedc047d97740e49276b6e409d9
bytegenious01 <bytegenius@infinitydevs.io>
2024-05-16 03:27:48 -0700

    feat: dumped user messages in the inbox

--- src/app/(auth)/inbox/page.tsx
+++ src/app/(auth)/inbox/page.tsx
@@ -1,11 +1,17 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { getReadInboxMessageByUserId } from '@/lib/inbox-messages';
+import { getUserProfile } from '@/lib/user';
 import ChatPanel from './panels/chat-panel';
 import InboxPanel from './panels/inbox-panel';
 import AcceptedTab from './tabs/accepted-tab';
 
-export default function Inbox() {
+export default async function Inbox() {
+  const user = await getUserProfile();
+  const messages = getReadInboxMessageByUserId(user.user_data.id as number);
   return (
     <div className="inbox flex flex-col p-4 lg:h-screen lg:flex-row">
+      {messages && <pre>{JSON.stringify(messages, null, 2)}</pre>}
+
       <Tabs defaultValue="accepted" className="w-full lg:hidden">
         <TabsList className="bg-primary-50 lg:bg-transparent">
           <TabsTrigger value="accepted">Accepted</TabsTrigger>
