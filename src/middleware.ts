b0dae28ca3c9ca6d74feae5fc4dc177f814091e6
bytegenious01 <bytegenius@infinitydevs.io>
2024-05-17 05:41:25 -0700

    fix: user cant go to onboarding if he has completed already

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,5 +24,13 @@ export default async function middleware(req: NextRequest) {
       const absoluteURL = new URL('/onboarding', req.nextUrl.origin);
       return NextResponse.redirect(absoluteURL.toString());
     }
+    if (
+      isAuthenticated &&
+      userProfile.user_data.is_onboarded &&
+      req.nextUrl.pathname.includes('onboarding')
+    ) {
+      const absoluteURL = new URL('/profile', req.nextUrl.origin);
+      return NextResponse.redirect(absoluteURL.toString());
+    }
   }
 }
