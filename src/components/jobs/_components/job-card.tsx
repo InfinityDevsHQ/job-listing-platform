fbc09037a71868321ebf7dbdec9a577b7853cbaf
sheraz4196 <sheraztoor321@gmail.com>
2024-06-03 13:23:58 +0500

    fix: added dividers

--- src/components/jobs/_components/job-card.tsx
+++ src/components/jobs/_components/job-card.tsx
@@ -18,31 +18,35 @@ export default function JobCard({ job }: JobProps) {
       onClick={() => setIsOpened(!isOpened)}
     >
       <JobCardHeader title={job.title || ''} is_hot={job.is_hot || false} company="Semrush" />
-      <div className="grid grid-cols-3 items-center font-semibold text-neutral-900 lg:grid-cols-6">
-        <span className="flex max-w-max items-center gap-2.5">
+      <div className="grid grid-cols-3 items-center justify-between font-semibold text-neutral-900 lg:flex">
+        <span className="flex items-center gap-2.5">
           <Clock className="h-2.5 w-3 lg:h-3 lg:w-3.5" />
           <span className="text-xxs capitalize lg:text-sm">{job.employment_type}</span>
         </span>
-        <span className="flex max-w-max items-center gap-2.5">
+        <span className="hidden h-full w-0.5 bg-gray-50 lg:inline-block"></span>
+        <span className="flex items-center gap-2.5">
           <Locate className="h-2.5 w-3 lg:h-3 lg:w-3.5" />
           <span className="text-xxs capitalize lg:text-sm">{job.city}</span>
         </span>
-        <span className="flex max-w-max items-center gap-2.5">
+        <span className="hidden h-full w-0.5 bg-gray-50 lg:inline-block"></span>
+        <span className="flex items-center gap-2.5 border-r-2 border-gray-50">
           <BaggageClaim className="h-2.5 w-3 lg:h-3 lg:w-3.5" />
           <span className="text-xxs capitalize lg:text-sm">1-3 Years</span>
         </span>
-
-        <span className="flex max-w-max items-center gap-2.5">
+        <span className="hidden h-full w-0.5 bg-gray-50 lg:inline-block"></span>
+        <span className="flex items-center gap-2.5 border-r-2 border-gray-50">
           <CircleDollarSign className="h-2.5 w-3 lg:h-3 lg:w-3.5" />
           <span className="text-xxs capitalize lg:text-sm">
             ${job.remuneration_from}-{job.remuneration_to}
           </span>
         </span>
-        <span className="flex max-w-max items-center gap-2.5">
+        <span className="hidden h-full w-0.5 bg-gray-50 lg:inline-block"></span>
+        <span className="flex items-center gap-2.5 border-r-2 border-gray-50">
           <Users className="h-2.5 w-3 lg:h-3 lg:w-3.5" />
           <span className="text-xxs capitalize lg:text-sm">{job.applicants}</span>
         </span>
-        <span className="flex max-w-max items-center gap-2.5">
+        <span className="hidden h-full w-0.5 bg-gray-50 lg:inline-block"></span>
+        <span className="flex items-center gap-2.5">
           <Calendar className="h-2.5 w-3 lg:h-3 lg:w-3.5" />
           <span className="text-xxs capitalize lg:text-sm">
             {formatDate(job.updated || '') || formatDate(job.created || '')}
