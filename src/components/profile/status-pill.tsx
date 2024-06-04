d7f8f245780fc43d8720cbf15cf475f8aacd5566
sheraz4196 <sheraztoor321@gmail.com>
2024-06-04 15:41:58 +0500

    fix: offline status colors

--- src/components/profile/status-pill.tsx
+++ src/components/profile/status-pill.tsx
@@ -10,14 +10,14 @@ export default function StatusPill({ online_status }: StatusPillProps) {
       className={cn(
         'flex items-center gap-2.5 self-center rounded-md border bg-white px-2 py-1 lg:px-4 lg:py-2',
         online_status === 'Offline'
-          ? 'border-red-500 text-red-500'
+          ? 'border-gray-500 text-gray-500'
           : 'border-secondary-500 text-secondary-500'
       )}
     >
       <span
         className={cn(
           'inline-block h-2 w-2 rounded-full lg:h-4 lg:w-4',
-          online_status === 'Offline' ? 'bg-red-500' : 'bg-secondary-500'
+          online_status === 'Offline' ? 'bg-gray-500' : 'bg-secondary-500'
         )}
       ></span>
       <span className="text-xs font-semibold lg:text-base">{online_status}</span>
