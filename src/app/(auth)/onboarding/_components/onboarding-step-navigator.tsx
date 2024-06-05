a20cb339063ad3ae9cbe664df6188c91f6993d23
sheraz4196 <sheraztoor321@gmail.com>
2024-06-05 12:12:01 +0500

    fix: tabs background

--- src/app/(auth)/onboarding/_components/onboarding-step-navigator.tsx
+++ src/app/(auth)/onboarding/_components/onboarding-step-navigator.tsx
@@ -2,7 +2,7 @@
 import { Button } from '@/components/ui/button-new';
 import { Tabs, TabsList } from '@/components/ui/tabs';
 import { useQueryParams } from '@/hooks/useQueryParams';
-import { useEffect } from 'react';
+import { cn } from '@/lib/utils';
 type OnboardingStepNavigatorProps = {
   step?: string;
 };
@@ -11,50 +11,59 @@ export default function OnboardingStepNavigator({ step }: OnboardingStepNavigato
   const handleTabSelect = (value: string) => {
     addQueryParams('step', value);
   };
-  useEffect(() => {
-    window.history.replaceState({}, document.title, window.location.pathname);
-    if (!step) {
-      window.history.replaceState({}, document.title, window.location.pathname);
-    }
-  }, [step]);
+  // useEffect(() => {
+  //   window.history.replaceState({}, document.title, window.location.pathname);
+  //   if (!step) {
+  //     window.history.replaceState({}, document.title, window.location.pathname);
+  //   }
+  // }, [step]);
   return (
     <Tabs className="mb-3">
-      <TabsList className="flex items-center bg-white">
+      <TabsList className="flex items-center bg-transparent">
         <Button
-          // onClick={() => handleTabSelect('location')}
-          variant={step === 'location' ? 'default' : 'outline'}
-          className="cursor-default !border-none"
+          onClick={() => handleTabSelect('location')}
+          variant={step === 'location' ? 'primary' : 'outline'}
+          className={cn('cursor-default !border-none', step !== 'location' ? 'bg-transparent' : '')}
         >
           1. Location
         </Button>
 
         <Button
-          // onClick={() => handleTabSelect('upload-cv')}
-          variant={step === 'upload-cv' ? 'default' : 'outline'}
-          className="cursor-default !border-none"
+          onClick={() => handleTabSelect('upload-cv')}
+          variant={step === 'upload-cv' ? 'primary' : 'outline'}
+          className={cn(
+            'cursor-default !border-none',
+            step !== 'upload-cv' ? 'bg-transparent' : ''
+          )}
         >
           2. Upload CV
         </Button>
 
         <Button
-          // onClick={() => handleTabSelect('filter-jobs')}
-          variant={step === 'filter-jobs' ? 'default' : 'outline'}
-          className="cursor-default !border-none"
+          onClick={() => handleTabSelect('filter-jobs')}
+          variant={step === 'filter-jobs' ? 'primary' : 'outline'}
+          className={cn(
+            'cursor-default !border-none',
+            step !== 'filter-jobs' ? 'bg-transparent' : ''
+          )}
         >
           3. Filter Jobs
         </Button>
 
         <Button
-          // onClick={() => handleTabSelect('contact')}
-          variant={step === 'contact' ? 'default' : 'outline'}
-          className="cursor-default !border-none"
+          onClick={() => handleTabSelect('contact')}
+          variant={step === 'contact' ? 'primary' : 'outline'}
+          className={cn('cursor-default !border-none', step !== 'contact' ? 'bg-transparent' : '')}
         >
           4. Contact
         </Button>
         <Button
-          // onClick={() => handleTabSelect('terms-and-conditions')}
-          variant={step === 'terms-and-conditions' ? 'default' : 'outline'}
-          className="cursor-default !border-none"
+          onClick={() => handleTabSelect('terms-and-conditions')}
+          variant={step === 'terms-and-conditions' ? 'primary' : 'outline'}
+          className={cn(
+            'cursor-default !border-none',
+            step !== 'terms-and-conditions' ? 'bg-transparent' : ''
+          )}
         >
           5. Finish
         </Button>
