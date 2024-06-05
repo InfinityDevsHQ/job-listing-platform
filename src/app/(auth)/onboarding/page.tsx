0ed1fb0b1f584f7d919a6ea721f1df3021b3f22a
sheraz4196 <sheraztoor321@gmail.com>
2024-06-05 12:47:36 +0500

    feat: added welcome tile on onboarding pages

--- src/app/(auth)/onboarding/page.tsx
+++ src/app/(auth)/onboarding/page.tsx
@@ -1,3 +1,4 @@
+import Hero from '@/app/(public)/companies/_components/hero';
 import FormsSlider from './_components/forms-slider';
 import OnboardingStepNavigator from './_components/onboarding-step-navigator';
 export default function Home({ searchParams }: { searchParams?: { step?: string } }) {
@@ -6,6 +7,13 @@ export default function Home({ searchParams }: { searchParams?: { step?: string 
     <>
       <div className="flex flex-col items-center justify-between p-4">
         {/* {step && <Intro />} */}
+        {step && (
+          <Hero
+            heading="Welcome! What now?"
+            className="self-stretch border border-primary-950"
+            desc={`Want to improve your job search? Start by completing your profile. It'll help AI find the perfect job for you.`}
+          />
+        )}
         {step && <OnboardingStepNavigator step={step} />}
         <FormsSlider step={step} />
       </div>
