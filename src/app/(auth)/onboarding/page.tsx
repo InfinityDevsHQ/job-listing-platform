7015ba47222795a6a14d60ab14d5883f502e6d55
sheraz4196 <sheraztoor321@gmail.com>
2024-06-05 12:57:04 +0500

    fix: gaps

--- src/app/(auth)/onboarding/page.tsx
+++ src/app/(auth)/onboarding/page.tsx
@@ -5,7 +5,7 @@ export default function Home({ searchParams }: { searchParams?: { step?: string 
   const step = searchParams?.step;
   return (
     <>
-      <div className="flex flex-col items-center justify-between p-4">
+      <div className="flex flex-col items-center justify-between gap-4 p-4 lg:gap-8">
         {/* {step && <Intro />} */}
         {step && (
           <Hero
