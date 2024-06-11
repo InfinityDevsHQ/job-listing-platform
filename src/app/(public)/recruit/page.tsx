d983ec7fb348e0549e1fb6a26bf0bb0a8da8791a
sheraz4196 <sheraztoor321@gmail.com>
2024-06-11 11:26:15 +0500

    refactor: new component for recruiter hero

--- src/app/(public)/recruit/page.tsx
+++ src/app/(public)/recruit/page.tsx
@@ -1,8 +1,8 @@
 import UserHeader from '@/components/gernal/user-header';
 import RecruitersCarousel from '@/components/recruit/recruiters-carousel';
+import RecruitersHero from '@/components/recruit/recruiters-hero';
 import { Button } from '@/components/ui/button-new';
 import InfoList from '@/components/ui/info-list';
-import { EyeIcon } from 'lucide-react';
 import Image from 'next/image';
 import FeatureCard from './_components/feature-card';
 
@@ -10,33 +10,7 @@ const Recruit = () => {
   return (
     <section className="bg-secondary-50 p-4 lg:gap-8 lg:p-8">
       <div className="relative flex w-full flex-col items-center gap-4 overflow-hidden rounded-md border border-neutral-200 bg-white p-4 lg:gap-8 lg:p-8">
-        <div className="relative flex w-full flex-col items-center gap-4 lg:flex-row lg:justify-between">
-          <div className="order-2 flex flex-col gap-4 lg:order-1 lg:gap-8">
-            <h1 className="text-center text-2xl font-bold text-gray-700 lg:text-left lg:text-5xl">
-              Rethink! The Way You Are
-            </h1>
-            <p className="max-w-2xl text-lg text-gray-500">
-              ClickJob is the all-in-one hiring solution that makes every step of your selection
-              process easier and lets you focus on what matters the most - your candidates.
-            </p>
-            <div className="flex flex-col items-center gap-3 lg:flex-row">
-              <Button variant="secondary" className="w-full lg:w-auto">
-                Try it Free for 14 Days
-              </Button>
-              <Button variant="outline" className="w-full lg:w-auto">
-                <EyeIcon className="mr-2 h-4 w-4" />
-                See it in Action
-              </Button>
-            </div>
-          </div>
-          <Image
-            src="/assets/images/recruit/clickjob_ai_recruit_hero_section.png"
-            width={306}
-            height={400}
-            alt="party"
-            className="order-1 h-40 w-40 lg:order-2 lg:h-96 lg:w-full lg:max-w-sm"
-          />
-        </div>
+        <RecruitersHero />
         <div className="flex w-full flex-col items-center gap-8">
           <h2 className="text-xl font-bold text-gray-700 lg:text-4xl">Top brands trust ClickJob</h2>
           <span className="text-gray-500">
---
+++ src/components/recruit/recruiters-hero.tsx
@@ -0,0 +1,34 @@
+import { EyeIcon } from 'lucide-react';
+import Image from 'next/image';
+import { Button } from '../ui/button-new';
+export default function RecruitersHero() {
+  return (
+    <div className="relative flex w-full flex-col items-center gap-4 lg:flex-row lg:justify-between">
+      <div className="order-2 flex flex-col gap-4 lg:order-1 lg:gap-8">
+        <h1 className="text-center text-2xl font-bold text-gray-700 lg:text-left lg:text-5xl">
+          Rethink! The Way You Are
+        </h1>
+        <p className="max-w-2xl text-lg text-gray-500">
+          ClickJob is the all-in-one hiring solution that makes every step of your selection process
+          easier and lets you focus on what matters the most - your candidates.
+        </p>
+        <div className="flex flex-col items-center gap-3 lg:flex-row">
+          <Button variant="secondary" className="w-full lg:w-auto">
+            Try it Free for 14 Days
+          </Button>
+          <Button variant="outline" className="w-full lg:w-auto">
+            <EyeIcon className="mr-2 h-4 w-4" />
+            See it in Action
+          </Button>
+        </div>
+      </div>
+      <Image
+        src="/assets/images/recruit/clickjob_ai_recruit_hero_section.png"
+        width={306}
+        height={400}
+        alt="party"
+        className="order-1 h-40 w-40 lg:order-2 lg:h-96 lg:w-full lg:max-w-sm"
+      />
+    </div>
+  );
+}
