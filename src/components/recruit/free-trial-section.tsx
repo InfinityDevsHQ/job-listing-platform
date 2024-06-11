aab1d1f8af3928984b0e326a249f681dc6c72d70
sheraz4196 <sheraztoor321@gmail.com>
2024-06-11 12:33:19 +0500

    refactor: better infolist structure

--- src/components/recruit/free-trial-section.tsx
+++ src/components/recruit/free-trial-section.tsx
@@ -1,17 +1,19 @@
 import { Button } from '../ui/button-new';
 import InfoList from '../ui/info-list';
 export default function FreeTrialSection() {
+  const FeaturesList = [
+    'Monthly contracts, cancel at any time',
+    'Book a Free Demo with our team',
+    `We'll do the setup for you`,
+  ];
   return (
     <section className="flex w-full flex-col items-center justify-center gap-8 bg-secondary-gradient p-4 lg:p-8">
       <h2 className="text-xl font-bold text-gray-700 lg:text-4xl">Start your 14-day free trial</h2>
       <p className="text-center text-gray-500">
         Join 270+ companies already growing with ClickJob.
       </p>
-      <InfoList className="text-center">
-        <li className="text-base text-gray-500">Monthly contracts, cancel at any time</li>
-        <li className="text-base text-gray-500">Book a Free Demo with our team</li>
-        <li className="text-base text-gray-500">We&apos;ll do the setup for you</li>
-      </InfoList>
+      <InfoList className="!text-center" listClassName="text-center mx-auto" list={FeaturesList} />
+
       <div className="flex w-full flex-col items-center justify-center gap-3 lg:flex-row">
         <Button variant="secondary" className="w-full lg:w-auto">
           Try it Free for 14 Days
--- src/components/ui/info-list.tsx
+++ src/components/ui/info-list.tsx
@@ -6,10 +6,18 @@ type InfoListProps = {
   children?: React.ReactNode;
   variant?: string;
   className?: string;
+  listClassName?: string;
   icon?: ReactElement;
 };
 
-const InfoList = ({ list, children, variant = 'primary', className, icon }: InfoListProps) => {
+const InfoList = ({
+  list,
+  children,
+  variant = 'primary',
+  className,
+  icon,
+  listClassName,
+}: InfoListProps) => {
   return (
     <ul className={cn('list flex list-inside flex-col gap-2 lg:gap-4', className)}>
       {children ? (
@@ -17,7 +25,10 @@ const InfoList = ({ list, children, variant = 'primary', className, icon }: Info
       ) : (
         <>
           {list?.map((listItem, index) => (
-            <li key={index} className="flex items-center text-base text-gray-500">
+            <li
+              key={index}
+              className={cn('flex items-center text-base text-gray-500', listClassName)}
+            >
               {icon && icon}
               {listItem}
             </li>
