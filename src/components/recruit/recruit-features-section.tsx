6aa0c323cb2a11493d1967345cee3a562fa67ce9
sheraz4196 <sheraztoor321@gmail.com>
2024-06-11 12:43:10 +0500

    refactor: improve list blocks code

--- src/components/recruit/recruit-features-section.tsx
+++ src/components/recruit/recruit-features-section.tsx
@@ -1,6 +1,31 @@
 import InfoList from '../ui/info-list';
 import FeatureCard from './feature-card';
 export default function RecruitFeaturesSection() {
+  const FeaturesListObe = [
+    `Generate text with a single click inside ClickJob's job editor`,
+    'Content is automatically split into specific fields',
+    'Make changes if necessary and enjoy the time savings',
+  ];
+  const FeaturesListTwo = [
+    'Explore our Workflow Automation feature and create automated actions tailored to your needs',
+    'Automate communication, CV screening, GDPR management, job ad posting, candidate assessment, and much more',
+    'Discover advanced AI-powered tools in ClickJob',
+  ];
+  const FeaturesListThree = [
+    'Import candidates directly from LinkedIn with our browser extension',
+    'Find and pre-evaluate top candidates easily through AI recommendations',
+    'Advertise jobs across multiple job boards',
+  ];
+  const FeaturesListFour = [
+    'Our AI-powered parsing tool scans even large volumes of CVs',
+    'All relevant details are automatically extracted from CVs',
+    'CV content is organized to categories like Personal Data, Skills, Job and EducationHistory',
+  ];
+  const FeaturesListFive = [
+    'Generate text with a single click inside ClickJob’s job editor',
+    'Content is automatically split into specific fields',
+    'Make changes if necessary and enjoy the time savings',
+  ];
   return (
     <section className="flex w-full flex-col gap-8">
       <h2 className="col-span-2 text-center text-xl font-bold text-gray-700 lg:text-4xl">
@@ -14,17 +39,7 @@ export default function RecruitFeaturesSection() {
           To engage qualified candidates, a compelling job description is crucial. To help you save
           time, ClickJob has launched AI-powered job description.
         </p>
-        <InfoList variant="secondary">
-          <li className="text-base text-gray-500">
-            Generate text with a single click inside ClickJob&apos;s job editor
-          </li>
-          <li className="text-base text-gray-500">
-            Content is automatically split into specific fields
-          </li>
-          <li className="text-base text-gray-500">
-            Make changes if necessary and enjoy the time savings
-          </li>
-        </InfoList>
+        <InfoList variant="secondary" list={FeaturesListObe} />
       </FeatureCard>
       <FeatureCard
         heading="Save time with AI and automation"
@@ -35,19 +50,7 @@ export default function RecruitFeaturesSection() {
           Say goodbye to tedious manual tasks, boost your team’s productivity and ensure positive
           candidate experience at every stage of the hiring journey.
         </p>
-        <InfoList variant="secondary">
-          <li className="text-base text-gray-500">
-            Explore our Workflow Automation feature and create automated actions tailored to your
-            needs
-          </li>
-          <li className="text-base text-gray-500">
-            Automate communication, CV screening, GDPR management, job ad posting, candidate
-            assessment, and much more
-          </li>
-          <li className="text-base text-gray-500">
-            Discover advanced AI-powered tools in ClickJob
-          </li>
-        </InfoList>
+        <InfoList variant="secondary" list={FeaturesListTwo} />
       </FeatureCard>
       <FeatureCard
         heading="Maximize your sourcing strategy"
@@ -57,15 +60,7 @@ export default function RecruitFeaturesSection() {
           Build a strong talent pool and find your next hires easier and faster with advanced
           sourcing features.
         </p>
-        <InfoList variant="secondary">
-          <li className="text-base text-gray-500">
-            Import candidates directly from LinkedIn with our browser extension
-          </li>
-          <li className="text-base text-gray-500">
-            Find and pre-evaluate top candidates easily through AI recommendations
-          </li>
-          <li className="text-base text-gray-500">Advertise jobs across multiple job boards</li>
-        </InfoList>
+        <InfoList variant="secondary" list={FeaturesListThree} />
       </FeatureCard>
       <FeatureCard
         heading="CV screening made simple"
@@ -76,18 +71,7 @@ export default function RecruitFeaturesSection() {
           Let AI technology turn all new job applications into detailed candidate profiles within
           seconds.
         </p>
-        <InfoList variant="secondary">
-          <li className="text-base text-gray-500">
-            Our AI-powered parsing tool scans even large volumes of CVs
-          </li>
-          <li className="text-base text-gray-500">
-            All relevant details are automatically extracted from CVs
-          </li>
-          <li className="text-base text-gray-500">
-            CV content is organized to categories like Personal Data, Skills, Job and Education
-            History
-          </li>
-        </InfoList>
+        <InfoList variant="secondary" list={FeaturesListFour} />
       </FeatureCard>
       <FeatureCard
         heading="AI-powered job descriptions with ChatGPT integration"
@@ -97,17 +81,7 @@ export default function RecruitFeaturesSection() {
           To engage qualified candidates, a compelling job description is crucial. To help you save
           time, ClickJob has launched AI-powered job description.
         </p>
-        <InfoList variant="secondary">
-          <li className="text-base text-gray-500">
-            Generate text with a single click inside ClickJob’s job editor
-          </li>
-          <li className="text-base text-gray-500">
-            Content is automatically split into specific fields
-          </li>
-          <li className="text-base text-gray-500">
-            Make changes if necessary and enjoy the time savings
-          </li>
-        </InfoList>
+        <InfoList variant="secondary" list={FeaturesListFive} />
       </FeatureCard>
     </section>
   );
