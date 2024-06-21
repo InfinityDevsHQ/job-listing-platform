550a46bb86382e2195c51c97d48745804737061c
sheraz4196 <sheraztoor321@gmail.com>
2024-06-21 12:45:14 +0500

    fix: header on mobiles

--- src/components/header/_components/mobile-nav.tsx
+++ src/components/header/_components/mobile-nav.tsx
@@ -1,19 +1,26 @@
+'use client';
 import { Button } from '@/components/ui/button-new';
+import useAuthStore from '@/stores/authStore/store';
 import { AlignJustify, BellIcon, MailIcon } from 'lucide-react';
 
 const MobileNav = () => {
+  const { isAuthenticated } = useAuthStore();
   return (
-    <div className="flex items-center gap-4 lg:hidden">
-      <Button variant="outline" size="icon-xs">
-        <MailIcon className="h-6 w-7" />
-      </Button>
-      <Button variant="outline" size="icon-xs">
-        <BellIcon className="h-6 w-7" />
-      </Button>
-      <Button variant="outline" size="icon-xs">
-        <AlignJustify className="h-7 w-7" />
-      </Button>
-    </div>
+    <>
+      {isAuthenticated && (
+        <div className="flex items-center gap-4 lg:hidden">
+          <Button variant="outline" size="icon-xs">
+            <MailIcon className="h-6 w-7" />
+          </Button>
+          <Button variant="outline" size="icon-xs">
+            <BellIcon className="h-6 w-7" />
+          </Button>
+          <Button variant="outline" size="icon-xs">
+            <AlignJustify className="h-7 w-7" />
+          </Button>
+        </div>
+      )}
+    </>
   );
 };
 
