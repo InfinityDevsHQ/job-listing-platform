7fe52394c90b3f7b4dc7959978ecc574074a1358
sheraz4196 <sheraztoor321@gmail.com>
2024-06-21 12:20:52 +0500

    refactor: separate component for searchings

---
+++ src/components/homepage/job-listing-search-bar.tsx
@@ -0,0 +1,11 @@
+import Hero from '@/app/(public)/companies/_components/hero';
+export default function JobListingSearchBard() {
+  return (
+    <Hero
+      heading="Find your dream job here!"
+      desc="Explore the latest job openings and apply for the best job opportunities available today!"
+      search
+      vectorUrl="/assets/images/home/homePage_hero_section_vector.png"
+    />
+  );
+}
