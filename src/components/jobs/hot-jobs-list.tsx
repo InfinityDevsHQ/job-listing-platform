62bfa830df2371b770f11f980ee3abebf80f02b4
sheraz4196 <sheraztoor321@gmail.com>
2024-06-21 16:18:03 +0500

    feat: search functionality

--- src/components/jobs/hot-jobs-list.tsx
+++ src/components/jobs/hot-jobs-list.tsx
@@ -2,6 +2,7 @@
 import { useGetJobs } from '@/app/utils/rq/hooks/use-jobs';
 
 import { cn } from '@/lib/utils';
+import useSearchQueryStore from '@/stores/searchQueryStore/store';
 import { RefreshCcwIcon } from 'lucide-react';
 import { JobCardSkeleton } from '../skeleton/job-card-skeleton';
 import { Button } from '../ui/button-new';
@@ -9,23 +10,24 @@ import JobCard from './_components/job-card';
 
 const HotJobs = () => {
   const { fetchNextPage, data, hasNextPage, isFetchingNextPage, isFetching } = useGetJobs(true);
-
+  const { query } = useSearchQueryStore();
   const jobs = data?.pages.flat() || [];
   const onClickLoadMore = () => {
     fetchNextPage();
   };
-
+  const filteredJobs = query
+    ? jobs.filter((job) => job.title?.toLowerCase().includes(query.toLowerCase()))
+    : jobs;
   if (isFetching && !isFetchingNextPage) {
     return <JobCardSkeleton />;
   }
-
   if (!jobs.length) {
     return <h3 className="mt-2 text-center text-sm font-semibold text-gray-900">No Jobs found</h3>;
   }
 
   return (
     <div className="flex flex-col gap-4 lg:gap-8">
-      {jobs?.map((job, index) => <JobCard key={index} job={job} />)}
+      {filteredJobs?.map((job, index) => <JobCard key={index} job={job} />)}
       {hasNextPage && (
         <div className="flex items-center justify-end">
           <Button onClick={onClickLoadMore} disabled={isFetchingNextPage} variant={'primary'}>
