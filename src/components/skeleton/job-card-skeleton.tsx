1f3e31f51be16c5e0408e01dc4f1ee58a7a63906
sheraz4196 <sheraztoor321@gmail.com>
2024-06-26 15:42:02 +0500

    fix: mobile skeleton

--- src/components/skeleton/job-card-skeleton.tsx
+++ src/components/skeleton/job-card-skeleton.tsx
@@ -11,23 +11,12 @@ export const JobCardSkeleton = () => {
               <Skeleton className="h-5 w-20 bg-gray-200" />
             </h3>
             <div className="flex w-full flex-col gap-4 lg:flex-row">
-              <div className="flex w-full flex-wrap items-center gap-3">
+              <div className="p grid w-full grid-cols-3 items-center gap-3 lg:grid-cols-6">
                 <Skeleton className="h-4 w-24 bg-gray-200" />
-                <svg viewBox="0 0 2 2" className="h-1 w-1 fill-current text-gray-200">
-                  <circle cx="1" cy="1" r="1" />
-                </svg>
                 <Skeleton className="h-4 w-14 bg-gray-200" />
-                <svg viewBox="0 0 2 2" className="h-1 w-1 fill-current text-gray-200">
-                  <circle cx="1" cy="1" r="1" />
-                </svg>
                 <Skeleton className="h-4 w-20 bg-gray-200" />
-                <svg viewBox="0 0 2 2" className="h-1 w-1 fill-current text-gray-200">
-                  <circle cx="1" cy="1" r="1" />
-                </svg>
-                <Skeleton className="h-4 w-16 bg-gray-200" />
-                <svg viewBox="0 0 2 2" className="h-1 w-1 fill-current text-gray-200">
-                  <circle cx="1" cy="1" r="1" />
-                </svg>
+                <Skeleton className="h-4 w-16" />
+                <Skeleton className="h-4 w-20 bg-gray-200" />
                 <Skeleton className="h-4 w-20 bg-gray-200" />
               </div>
             </div>
