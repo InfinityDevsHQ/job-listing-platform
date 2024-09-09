c1fd3ccc7c33cae1733c97d2bf01e7e4b71e18ae
Danish Asif <danishasifdev@gmail.com>
2024-09-09 16:12:17 +0500

    fix: updated hero

--- src/app/(public)/companies/_components/hero.tsx
+++ src/app/(public)/companies/_components/hero.tsx
@@ -2,7 +2,6 @@
 import Input from '@/components/ui/input';
 import { cn } from '@/lib/utils';
 import { SearchIcon } from 'lucide-react';
-import Image from 'next/image';
 type HeroProps = {
   heading: string;
   desc?: string;
@@ -26,7 +25,7 @@ const Hero = ({
   return (
     <div
       className={cn(
-        'z-10 grid grid-cols-3 items-center gap-4 overflow-hidden rounded-md bg-dark-primaryDark bg-cover p-4 lg:gap-8 lg:p-8',
+        'z-10 grid items-center gap-4 overflow-hidden rounded-md bg-dark-backgroundDark bg-cover p-4 lg:gap-8 lg:p-8',
         className
       )}
       // style={{
@@ -41,21 +40,23 @@ const Hero = ({
         )}
       >
         <h2 className="text-lg font-bold capitalize text-dark-nameWhite lg:text-3xl">{heading}</h2>
-        {desc && <p className="hidden text-base text-dark-nameWhite md:block">{desc}</p>}
+        {desc && <p className="hidden text-base text-dark-name md:block">{desc}</p>}
 
         {search && (
-          <Input
-            leadingIcon={<SearchIcon className="text-dark-name" />}
-            placeholder="Search"
-            onChange={(e) => {
-              if (setSearchQuery) setSearchQuery(e.target.value);
-              console.log(searchQuery);
-              onSearch && onSearch();
-            }}
-          />
+          <span className="w-[45%]">
+            <Input
+              leadingIcon={<SearchIcon className="text-dark-name" />}
+              placeholder="Search"
+              onChange={(e) => {
+                if (setSearchQuery) setSearchQuery(e.target.value);
+                console.log(searchQuery);
+                onSearch && onSearch();
+              }}
+            />
+          </span>
         )}
       </div>
-      {vectorUrl && (
+      {/* {vectorUrl && (
         <Image
           alt="Hero Image"
           src={vectorUrl}
@@ -64,7 +65,7 @@ const Hero = ({
           quality={100}
           className="ml-auto hidden lg:block"
         />
-      )}
+      )} */}
     </div>
   );
 };
